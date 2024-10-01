2ed6bdb2af6c473f5c2ca2224f109f7c29457233
yakire <53883914+gamenarkyt@users.noreply.github.com>
2024-10-01 21:12:07 +0300

    Add more icons for Sidebar

--- src/shared/icons/index.tsx
+++ src/shared/icons/index.tsx
@@ -1,3 +1,6 @@
 import ProfileIcon from "./svg/profile-icon.svg?react";
+import SearchIcon from "./svg/search-icon.svg?react";
+import SettingsIcon from "./svg/settings-icon.svg?react";
+import LogoutIcon from "./svg/logout-icon.svg?react";
 
-export { ProfileIcon };
+export { ProfileIcon, SearchIcon, SettingsIcon, LogoutIcon };
