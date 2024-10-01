7b5c3af4fccecbaebf377d7f698d49d3b44f9ac8
yakire <53883914+gamenarkyt@users.noreply.github.com>
2024-10-01 19:15:11 +0300

    Add border prop to BaseComponent

--- src/components/ui/BaseComponent/BaseComponent.tsx
+++ src/components/ui/BaseComponent/BaseComponent.tsx
@@ -5,15 +5,18 @@ interface IBaseComponent {
   className?: string;
   children?: React.ReactNode;
   touchable?: boolean;
+  border?: boolean;
 }
 
 export const BaseComponent: FC<IBaseComponent> = ({
   children,
   className,
   touchable = false,
+  border = false,
 }) => {
   const _class = clsx(styles.basecomponent, className, {
     [styles.touchable]: touchable,
+    [styles.border]: border,
   });
 
   return <div className={_class}>{children}</div>;
