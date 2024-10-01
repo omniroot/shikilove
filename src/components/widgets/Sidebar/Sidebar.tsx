c59954c4885cd735654ca79957209fc1bbba8e89
yakire <53883914+gamenarkyt@users.noreply.github.com>
2024-10-01 23:34:58 +0300

    Update Sidebar

--- src/components/widgets/Sidebar/Sidebar.tsx
+++ src/components/widgets/Sidebar/Sidebar.tsx
@@ -7,23 +7,44 @@ import {
   SettingsIcon,
 } from "@/shared/icons";
 import { IconButton } from "@ui/IconButton/IconButton";
+import { Link } from "react-router-dom";
+import { MouseEvent, useState } from "react";
 
 export const Sidebar = () => {
+  const [isSidebarHovered, setIsSidebarHovered] = useState(false);
+
+  const onSidebarMouseEnter = (event: MouseEvent<HTMLDivElement>) => {
+    setIsSidebarHovered(true);
+  };
+  const onSidebarMouseLeave = (event: MouseEvent<HTMLDivElement>) => {
+    setIsSidebarHovered(false);
+  };
+
   return (
-    <Box className={styles.sidebar} flexDirection="column">
-      <span>Shikimori</span>
-      <IconButton border="active">
-        <ProfileIcon />
-      </IconButton>
-      <IconButton>
-        <SearchIcon />
-      </IconButton>
-      <IconButton>
-        <SettingsIcon />
-      </IconButton>
-      <IconButton>
-        <LogoutIcon />
-      </IconButton>
+    <Box className={styles.sidebar_container} padding="none" border="none">
+      <Box
+        className={styles.sidebar}
+        flexDirection="column"
+        alignItems="center"
+        gap="1"
+        onMouseEnter={onSidebarMouseEnter}
+        onMouseLeave={onSidebarMouseLeave}
+      >
+        {/* <span>Shikimori</span> */}
+        <IconButton border="active">
+          <ProfileIcon />
+          {isSidebarHovered && "Profile"}
+        </IconButton>
+        <IconButton border="transparent">
+          <SearchIcon />
+        </IconButton>
+        <IconButton border="transparent">
+          <SettingsIcon />
+        </IconButton>
+        <IconButton border="transparent">
+          <LogoutIcon />
+        </IconButton>
+      </Box>
     </Box>
   );
 };
