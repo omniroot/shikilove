4d85d294f13ce008d80c9864426d6849e196d80d
yakire <53883914+gamenarkyt@users.noreply.github.com>
2024-10-01 20:00:02 +0300

    Add radius prop to BaseComponent

--- src/components/ui/BaseComponent/BaseComponent.tsx
+++ src/components/ui/BaseComponent/BaseComponent.tsx
@@ -6,6 +6,7 @@ interface IBaseComponent {
   children?: React.ReactNode;
   touchable?: boolean;
   border?: boolean | "default" | "active";
+  radius?: boolean | "1" | "2";
 }
 
 export const BaseComponent: FC<IBaseComponent> = ({
@@ -13,11 +14,14 @@ export const BaseComponent: FC<IBaseComponent> = ({
   className,
   touchable = false,
   border = false,
+  radius = false,
 }) => {
   const _class = clsx(styles.basecomponent, className, {
     [styles.touchable]: touchable,
     [styles.border_default]: border === "default",
     [styles.border_active]: border === "active",
+    [styles.radius_1]: radius === "1",
+    [styles.radius_2]: radius === "2",
   });
 
   return <div className={_class}>{children}</div>;
