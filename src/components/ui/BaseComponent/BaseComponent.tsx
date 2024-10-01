5a3cba441d3c058946103944421259d7b2708ac6
yakire <53883914+gamenarkyt@users.noreply.github.com>
2024-10-01 23:34:31 +0300

    Many update for BaseComponent, like gap, as, onMouse and etc

--- src/components/ui/BaseComponent/BaseComponent.tsx
+++ src/components/ui/BaseComponent/BaseComponent.tsx
@@ -1,45 +1,64 @@
-import { FC } from "react";
+import React, { FC } from "react";
 import styles from "./BaseComponent.module.scss";
 import clsx from "clsx";
 import { css } from "@emotion/css";
+import { Link } from "react-router-dom";
 
 export interface IBaseComponent {
   className?: string;
+  as?: React.ElementType | "div" | typeof Link;
   children?: React.ReactNode;
   touchable?: boolean;
-  border?: boolean | "default" | "active";
-  radius?: boolean | "1" | "2";
-  padding?: boolean | "1" | "2";
+  border?: "none" | "transparent" | "default" | "active";
+  radius?: "none" | "1" | "2";
+  padding?: "none" | "1" | "2";
   flexDirection?: "row" | "column";
   justifyContent?: "start" | "center" | "end";
   alignItems?: "start" | "center" | "end";
+  gap?: "none" | "1" | "2";
+  onMouseEnter?: (event: React.MouseEvent<HTMLDivElement>) => void;
+  onMouseLeave?: (event: React.MouseEvent<HTMLDivElement>) => void;
 }
 
 export const BaseComponent: FC<IBaseComponent> = ({
   children,
   className,
   touchable = false,
-  border = false,
-  radius = false,
-  padding = false,
+  border = "none",
+  radius = "none",
+  padding = "none",
   flexDirection = "row",
   justifyContent = "start",
   alignItems = "start",
+  gap = "none",
+  as = "div",
+  ...rest
 }) => {
   const _style = css`
     ${touchable ? "cursor: pointer;" : "cursor: default;"}
+    ${touchable && ":hover {border: var(--border-default);}"}
+    ${border === "none" && "border: var(--border-none);"}
+    ${border === "transparent" && "border: var(--border-transparent);"}
     ${border === "default" && "border: var(--border-default);"}
     ${border === "active" && "border: var(--border-active);"}
     ${radius === "1" && "border-radius: var(--radius-1);"}
     ${radius === "2" && "border-radius: var(--radius-2);"}
     ${padding === "1" && "padding: var(--padding-1);"}
     ${padding === "2" && "padding: var(--padding-2);"}
+    ${gap === "1" && "gap: var(--gap-1);"}
+    ${gap === "2" && "gap: var(--gap-2);"}
+
     flex-direction: ${flexDirection};
     justify-content: ${justifyContent};
     align-items: ${alignItems};
   `;
 
-  const _class = clsx(styles.basecomponent, _style, className);
+  const _class = clsx(className, styles.basecomponent, _style);
+  const Component = as;
 
-  return <div className={_class}>{children}</div>;
+  return (
+    <Component {...rest} className={_class}>
+      {children}
+    </Component>
+  );
 };
