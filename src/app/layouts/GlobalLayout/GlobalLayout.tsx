3275f68053a099d4b40378f82cbbbeea22825e74
yakire <53883914+gamenarkyt@users.noreply.github.com>
2024-10-01 23:33:26 +0300

    Update GlobalLayout

--- src/app/layouts/GlobalLayout/GlobalLayout.tsx
+++ src/app/layouts/GlobalLayout/GlobalLayout.tsx
@@ -1,12 +1,15 @@
 import { Outlet } from "react-router-dom";
 import styles from "./GlobalLayout.module.scss";
 import { Sidebar } from "@widgets/Sidebar/Sidebar";
+import { Box } from "@ui/Box/Box";
 
 export const GlobalLayout = () => {
   return (
     <div className={styles.globallayout}>
       <Sidebar />
-      <Outlet />
+      <Box className={styles.main}>
+        <Outlet />
+      </Box>
     </div>
   );
 };
