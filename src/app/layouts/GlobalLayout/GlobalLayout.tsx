206c18bda122a9241fd071e228123a48a991250c
yakire <53883914+gamenarkyt@users.noreply.github.com>
2024-10-01 19:09:53 +0300

    Add GlobalLayout component

--- src/app/layouts/GlobalLayout/GlobalLayout.tsx
+++ src/app/layouts/GlobalLayout/GlobalLayout.tsx
@@ -1,8 +1,11 @@
 import { Outlet } from "react-router-dom";
+import styles from "./GlobalLayout.module.scss";
+import { Sidebar } from "@widgets/Sidebar/Sidebar";
 
 export const GlobalLayout = () => {
   return (
-    <div>
+    <div className={styles.globallayout}>
+      <Sidebar />
       <Outlet />
     </div>
   );
