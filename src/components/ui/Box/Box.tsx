6a807041403281c521a7047720efe3fd39250673
yakire <53883914+gamenarkyt@users.noreply.github.com>
2024-10-01 20:21:36 +0300

    Update Box component

--- src/components/ui/Box/Box.tsx
+++ src/components/ui/Box/Box.tsx
@@ -1,5 +1,19 @@
+import { BaseComponent, IBaseComponent } from "@ui/BaseComponent/BaseComponent";
 import styles from "./Box.module.scss";
+import { FC } from "react";
 
-export const Box = () => {
-  return <div>Box</div>;
+interface IBoxProps extends IBaseComponent {}
+
+export const Box: FC<IBoxProps> = ({
+  children,
+  border = "default",
+  radius = "1",
+  padding = "1",
+  ...rest
+}) => {
+  return (
+    <BaseComponent {...rest} border={border} radius={radius} padding={padding}>
+      {children}
+    </BaseComponent>
+  );
 };
