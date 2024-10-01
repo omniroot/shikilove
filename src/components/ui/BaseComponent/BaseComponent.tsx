d5208876d521566cc27f8b76c57b70a507760a0d
yakire <53883914+gamenarkyt@users.noreply.github.com>
2024-10-01 20:05:04 +0300

    Add padding prop to BaseComponent

--- src/components/ui/BaseComponent/BaseComponent.tsx
+++ src/components/ui/BaseComponent/BaseComponent.tsx
@@ -7,6 +7,7 @@ interface IBaseComponent {
   touchable?: boolean;
   border?: boolean | "default" | "active";
   radius?: boolean | "1" | "2";
+  padding?: boolean | "1" | "2";
 }
 
 export const BaseComponent: FC<IBaseComponent> = ({
@@ -15,6 +16,7 @@ export const BaseComponent: FC<IBaseComponent> = ({
   touchable = false,
   border = false,
   radius = false,
+  padding = false,
 }) => {
   const _class = clsx(styles.basecomponent, className, {
     [styles.touchable]: touchable,
@@ -22,6 +24,8 @@ export const BaseComponent: FC<IBaseComponent> = ({
     [styles.border_active]: border === "active",
     [styles.radius_1]: radius === "1",
     [styles.radius_2]: radius === "2",
+    [styles.padding_1]: padding === "1",
+    [styles.padding_2]: padding === "2",
   });
 
   return <div className={_class}>{children}</div>;
