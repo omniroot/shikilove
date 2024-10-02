2ab6edc49208fc09a5c7614c1634cf19868d2242
omniroot <omnirootofc@gmail.com>
2024-10-02 21:35:01 +0300

    ProfilePage init

--- src/app/pages/ProfilePage/ProfilePage.tsx
+++ src/app/pages/ProfilePage/ProfilePage.tsx
@@ -1,31 +1,26 @@
 import { BaseComponent } from "@ui/BaseComponent/BaseComponent";
 import { Box } from "@ui/Box/Box";
+import { Divide } from "@ui/Divide/Divide";
 import { Test } from "@ui/Test/Test";
 
 export const ProfilePage = () => {
   return (
-    <div>
-      <span>ProfilePage</span>
-      <Box>Hello world</Box>
-      <Box>Subscribe</Box>
-      <Test justifyContent="center" alignItems="center">
-        123
-      </Test>
-      <Test justifyContent="center" alignItems="center">
-        123
-      </Test>
-      <Test justifyContent="center" alignItems="center">
-        123
-      </Test>
-      <Test justifyContent="center" alignItems="center">
-        123
-      </Test>
-      <Test justifyContent="center" alignItems="center">
-        123
-      </Test>
-      <Test justifyContent="center" alignItems="center">
-        123
-      </Test>
-    </div>
+    <Box border="none" padding="none" width="100%">
+      <Box width="100%">
+        <Box width="100%" border="none" padding="none">
+          <img src="avatar.png" width={200} />
+          <Divide orientation="vertical" width="170px" />
+          <Box flexDirection="column" width="100%" border="none" padding="none">
+            <Box justifyContent="space-between" width="100%" border="none">
+              <span>OmniRoot</span>
+              <span>около 5 часов назад</span>
+            </Box>
+            <Box justifyContent="space-between" width="100%" border="none">
+              <span>I love anime</span>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
   );
 };
