1b7759fdbbf25f74889c65a108b015973cec3cc5
omniroot <omnirootofc@gmail.com>
2024-10-07 17:59:17 +0300

    Add clamp prop to Typography

--- src/components/ui/Typography/Typography.tsx
+++ src/components/ui/Typography/Typography.tsx
@@ -7,21 +7,35 @@ import { FC } from "react";
 interface ITypographyProps extends IBaseComponent {
   size?: "1" | "2" | "3" | "4" | "5" | "6";
   weight?: "normal" | "bold";
+  clamp?: "none" | string;
+  textAlign?: "none" | "left" | "center" | "right";
 }
 
 export const Typography: FC<ITypographyProps> = ({
   className,
   size = "5",
+  weight = "normal",
+  clamp = "none",
+  textAlign = "none",
   children,
   ...rest
 }) => {
   const _style = css`
     ${`font-size: var(--text-size-h${size});`}
+    ${`font-weight: ${weight};`}
+    ${`text-align: ${textAlign};`}
+
+    ${clamp !== "none" &&
+    `display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: ${clamp};
+    overflow: hidden;
+    text-overflow: ellipsis;`}
   `;
 
   const _class = clsx(_style, className);
   return (
-    <BaseComponent {...rest} className={_class}>
+    <BaseComponent className={_class} {...rest} as="span">
       {children}
     </BaseComponent>
   );
