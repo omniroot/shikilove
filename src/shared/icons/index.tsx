4914d4c74773c9ecef4422bb28a5774bc979cd4b
omniroot <omnirootofc@gmail.com>
2024-10-07 19:13:59 +0300

    Add icons

--- src/shared/icons/index.tsx
+++ src/shared/icons/index.tsx
@@ -4,3 +4,11 @@ import SettingsIcon from "./svg/settings-icon.svg?react";
 import LogoutIcon from "./svg/logout-icon.svg?react";
 
 export { ProfileIcon, SearchIcon, SettingsIcon, LogoutIcon };
+
+import WatchingIcon from "./svg/watching-icon.svg?react";
+import PlannedIcon from "./svg/planned-icon.svg?react";
+import WatchedIcon from "./svg/watched-icon.svg?react";
+import PostponedIcon from "./svg/postponed-icon.svg?react";
+import DroppedIcon from "./svg/dropped-icon.svg?react";
+
+export { WatchingIcon, PlannedIcon, WatchedIcon, PostponedIcon, DroppedIcon };
