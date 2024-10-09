d8b3a8f715e5a9f6ed465b72f327295b16efd00b
omniroot <omnirootofc@gmail.com>
2024-10-10 02:17:24 +0300

    Add styles to Button component

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,24 +1,37 @@
 import { css } from "@emotion/css";
+import { IBaseComponent } from "@ui/BaseComponent/BaseComponent";
+import { Box } from "@ui/Box/Box";
 import { FC } from "react";
 
-interface IButtonProps {
+interface IButtonProps extends IBaseComponent {
   children?: React.ReactNode;
 }
-export const Button: FC<IButtonProps> = ({ children }) => {
+export const Button: FC<IButtonProps> = ({ children, ...rest }) => {
   const _style = css`
     padding: var(--padding);
     border-radius: calc(var(--radius) / 1.6);
+    color: var(--color-primary);
     background-color: var(--color-accent);
-    color: var(--color-text);
     border: none;
     cursor: pointer;
     transition: all 200ms;
+    font-size: var(--text-size-h4);
     font-family: inherit;
+    font-weight: bold;
 
     :active {
       transform: scale(0.95);
     }
   `;
 
-  return <button className={_style}>{children}</button>;
+  return (
+    <Box
+      className={_style}
+      {...rest}
+      backgroundColor="accent"
+      textColor="primary"
+    >
+      {children}
+    </Box>
+  );
 };
