de21e75673a2f192acb1f9f58f9680faf0771b16
omniroot <omnirootofc@gmail.com>
2024-10-10 02:13:55 +0300

    Add refreshToken and redirect to Login Page to GlobalLayout

--- src/app/layouts/GlobalLayout/GlobalLayout.tsx
+++ src/app/layouts/GlobalLayout/GlobalLayout.tsx
@@ -1,13 +1,36 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import styles from "./GlobalLayout.module.scss";
 import { Sidebar } from "@widgets/Sidebar/Sidebar";
 import { Box } from "@ui/Box/Box";
 import { useFetchCurrentUser } from "@/shared/hooks/useFetchCurrentUser";
+import { LoginPage } from "@/app/pages/LoginPage/LoginPage";
+import { useAuthorization } from "@/shared/hooks/useAuthorization";
 
 export const GlobalLayout = () => {
-  const { loading, ...rest } = useFetchCurrentUser();
+  const { loading, error, ...rest } = useFetchCurrentUser();
+  const { refreshTokens } = useAuthorization();
+
+  const refreshAndSaveTokens = async () => {
+    const respose = await refreshTokens();
+    if (respose) {
+      localStorage.setItem("access_token", respose.access_token);
+      localStorage.setItem("refresh_token", respose.refresh_token);
+      window.location.reload();
+      return true;
+    } else {
+      return false;
+    }
+  };
 
   if (loading) return "loading...";
+  if (error?.networkError?.message.includes("401") === true) {
+    if (localStorage.getItem("refresh_token")) {
+      refreshAndSaveTokens();
+      return null;
+    }
+    return <LoginPage />;
+  }
+
   console.log("@ loading", loading);
   console.log("@ rest", rest);
   return (
