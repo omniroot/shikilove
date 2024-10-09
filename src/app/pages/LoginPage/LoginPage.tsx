e39133a22da6c148f62cd5bce6969a3b5da7142c
omniroot <omnirootofc@gmail.com>
2024-10-10 02:13:15 +0300

    Add Login Page

---
+++ src/app/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,53 @@
+import { CONSTS } from "@/shared/consts/consts";
+import { useAuthorization } from "@/shared/hooks/useAuthorization";
+import { Box } from "@ui/Box/Box";
+import { Button } from "@ui/Button/Button";
+import { Typography } from "@ui/Typography/Typography";
+import { useSearchParams } from "react-router-dom";
+
+export const LoginPage = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const { fetchTokens } = useAuthorization();
+  const code = searchParams.get("code");
+
+  const getAndSaveTokens = async (code: string) => {
+    const response = await fetchTokens(code);
+    if (response) {
+      localStorage.setItem("access_token", response.access_token);
+      localStorage.setItem("refresh_token", response.refresh_token);
+      window.open(CONSTS.URL, "_self");
+    }
+  };
+
+  if (code) {
+    getAndSaveTokens(code);
+  }
+
+  const onLoginButtonClick = () => {
+    window.open(CONSTS.OAUTH_URL, "_self");
+  };
+  return (
+    <Box
+      justifyContent="center"
+      alignItems="center"
+      flexDirection="column"
+      gap="2"
+      padding="2"
+      width="100%"
+      height="100%"
+    >
+      <Typography size="1">LoginPage</Typography>
+      <Box
+        width="100%"
+        height="100%"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        gap="1"
+      >
+        <Typography size="3">Login with</Typography>
+        <Button onClick={onLoginButtonClick}>Shikimori</Button>
+      </Box>
+    </Box>
+  );
+};
--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,4 +1,5 @@
 import { GlobalLayout } from "@/app/layouts/GlobalLayout/GlobalLayout";
+import { LoginPage } from "@/app/pages/LoginPage/LoginPage";
 import { LogoutPage } from "@/app/pages/LogoutPage/LogoutPage";
 import { NotFoundPage } from "@/app/pages/NotFoundPage/NotFoundPage";
 import { ProfilePage } from "@/app/pages/ProfilePage/ProfilePage";
@@ -6,6 +7,7 @@ import { SearchPage } from "@/app/pages/SearchPage/SearchPage";
 import { SettingsPage } from "@/app/pages/SettingsPage/SettingsPage";
 import {
   createBrowserRouter,
+  createRoutesFromChildren,
   createRoutesFromElements,
   Route,
 } from "react-router-dom";
@@ -16,6 +18,7 @@ export const router = createBrowserRouter(
       <Route path="/" element={<ProfilePage />} />
       <Route path="/search" element={<SearchPage />} />
       <Route path="/settings" element={<SettingsPage />} />
+      <Route path="/login" element={<LoginPage />} />
       <Route path="/logout" element={<LogoutPage />} />
       <Route path="*" element={<NotFoundPage />} />
     </Route>
