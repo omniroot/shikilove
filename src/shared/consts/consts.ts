ff11be269db3860e6e8b8ad3d5b176a9d9c2a644
omniroot <omnirootofc@gmail.com>
2024-10-10 03:03:01 +0300

    Add miss CONSTS for OAUTH apps

--- src/shared/consts/consts.ts
+++ src/shared/consts/consts.ts
@@ -1,10 +1,10 @@
 export const CONSTS = {
-  API_URL: "https://shikimori.one/api/graphql",
-  ACCESS_TOKEN: "7gCPwwfuZ2UHXteRvBGqeDSpdqvPoL5EJwCjAhIB4bY",
-  URL: "http://localhost:5173/",
-  USER_ID: "1026254",
-  OAUTH_URL:
-    "https://shikimori.one/oauth/authorize?client_id=C0cTq8ZDkHvhldEsKgxlCXam--xNTC0nk6db3EsFozI&redirect_uri=http%3A%2F%2Flocalhost%3A5173%2Flogin%2F&response_type=code&scope=user_rates+comments+topics",
+  API_URL: import.meta.env.VITE_API_URL,
+  URL: import.meta.env.VITE_URL,
+  OAUTH_URL: import.meta.env.VITE_OAUTH_URL,
+  OAUTH_CLIENT_ID: import.meta.env.VITE_OAUTH_CLIENT_ID,
+  OAUTH_CLIENT_SECRET: import.meta.env.VITE_OAUTH_CLIENT_SECRET,
+  OAUTH_REDIRECT_URI: import.meta.env.VITE_OAUTH_REDIRECT_URI,
 };
 
 // graphql
