e1d7f30b22f40a7afe32fe3301c0d92be3abb3ef
omniroot <omnirootofc@gmail.com>
2024-10-12 00:48:14 +0300

    Rewrite IconButton component

--- src/components/ui/IconButton/IconButton.tsx
+++ src/components/ui/IconButton/IconButton.tsx
@@ -1,34 +1,10 @@
-import {
-	BaseComponent,
-	type IBaseComponent,
-} from "@ui/BaseComponent/BaseComponent";
 import type { FC } from "react";
+import styles from "./IconButton.module.scss";
 
-interface IIconButtonProps extends IBaseComponent {}
+interface IIconButtonProps {
+	children: React.ReactNode;
+}
 
-export const IconButton: FC<IIconButtonProps> = ({
-	children,
-	border = "none",
-	radius = "1",
-	padding = "1",
-	justifyContent = "center",
-	alignItems = "center",
-	gap = "1",
-	clickable = true,
-	...rest
-}) => {
-	return (
-		<BaseComponent
-			{...rest}
-			border={border}
-			radius={radius}
-			padding={padding}
-			justifyContent={justifyContent}
-			alignItems={alignItems}
-			gap={gap}
-			clickable={clickable}
-		>
-			{children}
-		</BaseComponent>
-	);
+export const IconButton: FC<IIconButtonProps> = ({ children }) => {
+	return <div className={styles.icon_button}>{children}</div>;
 };
