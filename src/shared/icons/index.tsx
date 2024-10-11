fff66bb56822f2a0f726ef968a733f224691657e
omniroot <omnirootofc@gmail.com>
2024-10-12 00:47:41 +0300

    Add AnimePageIcon

--- src/shared/icons/index.tsx
+++ src/shared/icons/index.tsx
@@ -1,12 +1,16 @@
+
+
+// Site Page Icons
 import ProfileIcon from "./svg/profile-icon.svg?react";
+import AnimeIcon from './svg/anime-page-icon.svg?react'
 import SearchIcon from "./svg/search-icon.svg?react";
 import SettingsIcon from "./svg/settings-icon.svg?react";
 import LogoutIcon from "./svg/logout-icon.svg?react";
 
-export { ProfileIcon, SearchIcon, SettingsIcon, LogoutIcon };
+export { ProfileIcon, AnimeIcon, SearchIcon, SettingsIcon, LogoutIcon };
 
 
-// Aime User Rate Filter Icons
+// Anime User Rate Filter Icons
 import WatchingIcon from "./svg/watching-icon.svg?react";
 import PlannedIcon from "./svg/planned-icon.svg?react";
 import WatchedIcon from "./svg/watched-icon.svg?react";
