cdee16a54fa161d6433d504047e3174200589720
omniroot <omnirootofc@gmail.com>
2024-10-14 02:12:20 +0300

    Commit RLInk component

--- src/components/ui/RLink/RLink.tsx
+++ src/components/ui/RLink/RLink.tsx
@@ -1,46 +1,41 @@
-import { css } from "@emotion/css";
-import {
-	BaseComponent,
-	type IBaseComponent,
-} from "@ui/BaseComponent/BaseComponent";
-import clsx from "clsx";
-import type { FC } from "react";
-import { Link, useLocation } from "react-router-dom";
+// import { css } from "@emotion/css";
+// import { Flex } from "@kuma-ui/core";
+// import clsx from "clsx";
+// import type { FC, ReactNode } from "react";
+// import { Link, useLocation } from "react-router-dom";
 
-interface IRLinkProps extends IBaseComponent {}
+// interface IRLinkProps {
+// 	children: ReactNode;
+// 	to: string;
+// }
 
-export const RLink: FC<IRLinkProps> = ({
-	children,
-	className,
-	clickable = true,
-	as = Link,
-	border = "transparent",
-	to,
-}) => {
-	const isCurrentPage = useLocation().pathname === to;
+// export const RLink: FC<IRLinkProps> = ({ children, to }) => {
+// 	const isCurrentPage = useLocation().pathname === to;
 
-	const _style = css`
-    color: inherit;
-    text-decoration: none;
+// 	const _style = css`
+//     color: inherit;
+//     text-decoration: none;
 
-    :hover {
-      color: var(--text-secondary);
-    }
+//     :hover {
+//       color: var(--text-secondary);
+//     }
 
-    ${isCurrentPage && "color: var(--accent);"}
-  `;
+//     ${isCurrentPage && "color: var(--accent);"}
+//   `;
 
-	const _class = clsx(className, _style);
+// 	const _class = clsx(_style);
 
-	return (
-		<BaseComponent
-			as={as}
-			to={to}
-			clickable={clickable}
-			border={border}
-			className={_class}
-		>
-			{children}
-		</BaseComponent>
-	);
-};
+// 	return (
+// 		<Flex
+// 			as={Link}
+// 			to={to}
+// 			bgColor="var(--color-secondary)"
+// 			padding={"var(--padding)"}
+// 			borderRadius={"var(--radius)"}
+// 			justifyContent={"center"}
+// 			alignItems={"center"}
+// 		>
+// 			{children}
+// 		</Flex>
+// 	);
+// };
