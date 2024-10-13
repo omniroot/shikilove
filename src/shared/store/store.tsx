36a03799bb3965b0ff0e471662c4d9deea12e3a8
omniroot <omnirootofc@gmail.com>
2024-10-13 18:01:29 +0300

    Add Global store for storing sidebar state

---
+++ src/shared/store/store.tsx
@@ -0,0 +1,16 @@
+import { create } from "zustand";
+
+interface IGlobalStore {
+  isRightSidebarOpened: boolean;
+  toggleRightSidebar: () => void;
+  rightSidebarContent: React.ReactNode;
+  setRightSidebarContent: (content: React.ReactNode) => void;
+}
+export const useGlobalStore = create<IGlobalStore>((set) => ({
+  isRightSidebarOpened: false,
+  toggleRightSidebar: () =>
+    set((state) => ({ isRightSidebarOpened: !state.isRightSidebarOpened })),
+  rightSidebarContent: (<div>Content</div>),
+  setRightSidebarContent: (content) => set(() => ({ rightSidebarContent: content })),
+}))
+
