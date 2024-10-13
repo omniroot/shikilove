2dd80d1d13cf84941a89947ad71a00e53afcbb64
omniroot <omnirootofc@gmail.com>
2024-10-13 18:01:57 +0300

    Use dayjs for lastOnlineAt in UserInfoCard

--- src/components/features/UserInfoCard/UserInfoCard.tsx
+++ src/components/features/UserInfoCard/UserInfoCard.tsx
@@ -1,9 +1,12 @@
 import { useFetchCurrentUser } from "@/shared/hooks/useFetchCurrentUser";
 import { ImageView } from "@ui/ImageView/ImageView";
 import styles from "./UserInfoCard.module.scss";
+import dayjs from "dayjs";
 
 export const UserInfoCard = () => {
 	const { nickname, lastOnlineAt, avatarUrl } = useFetchCurrentUser();
+
+	const _lastOnline = dayjs(lastOnlineAt).fromNow();
 
 	return (
 		<div className={styles.user_info_card}>
@@ -12,7 +15,7 @@ export const UserInfoCard = () => {
 			<div className={styles.info}>
 				<div className={styles.first_line}>
 					<span className={styles.nickname}>{nickname}</span>
-					<span className={styles.last_online_at}>{lastOnlineAt}</span>
+					<span className={styles.last_online_at}>{_lastOnline}</span>
 				</div>
 				<div className={styles.second_line}>
 					<span className={styles.about}>I love anime</span>
