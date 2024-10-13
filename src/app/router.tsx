03fe59743720f9c899c1b2772bbfbef2270db42e
omniroot <omnirootofc@gmail.com>
2024-10-13 18:06:13 +0300

    Add Search, Animes, Test  pages to router

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -2,11 +2,10 @@ import { GlobalLayout } from "@/app/layouts/GlobalLayout/GlobalLayout";
 import { AnimePage } from "@/app/pages/AnimePage/AnimePage";
 import { AnimesPage } from "@/app/pages/AnimesPage/AnimesPage";
 import { LoginPage } from "@/app/pages/LoginPage/LoginPage";
-import { LogoutPage } from "@/app/pages/LogoutPage/LogoutPage";
 import { NotFoundPage } from "@/app/pages/NotFoundPage/NotFoundPage";
 import { ProfilePage } from "@/app/pages/ProfilePage/ProfilePage";
 import { SearchPage } from "@/app/pages/SearchPage/SearchPage";
-import { SettingsPage } from "@/app/pages/SettingsPage/SettingsPage";
+import { TestPage } from "@/app/pages/TestPage/TestPage";
 import {
 	Route,
 	createBrowserRouter,
@@ -18,13 +17,13 @@ export const router = createBrowserRouter(
 		<Route path="/" element={<GlobalLayout />}>
 			<Route path="/" element={<ProfilePage />} />
 			<Route path="/search" element={<SearchPage />} />
-			<Route path="/animes" element={<AnimesPage />} />
+			<Route path="/animes/" element={<AnimesPage />} />
 			<Route path="/animes/:animeId" element={<AnimePage />} />
 
-			{/* <Route path="/test" element={<TestPage />} /> */}
-			<Route path="/settings" element={<SettingsPage />} />
+			<Route path="/test" element={<TestPage />} />
+			{/* <Route path="/settings" element={<SettingsPage />} /> */}
 			<Route path="/login" element={<LoginPage />} />
-			<Route path="/logout" element={<LogoutPage />} />
+			{/* <Route path="/logout" element={<LogoutPage />} /> */}
 			<Route path="*" element={<NotFoundPage />} />
 		</Route>,
 	),
