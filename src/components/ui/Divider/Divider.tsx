a75b2d0d9d32a8e8a7f0d3a59b25b67b067cad5c
omniroot <omnirootofc@gmail.com>
2024-10-13 18:43:48 +0300

    Rewrite and rename Divider

---
+++ src/components/ui/Divider/Divider.tsx
@@ -0,0 +1,15 @@
+import styles from './Divider.module.scss'
+import type { FC } from "react";
+
+interface IDivideProps {
+	orientation?: "horizontal" | "vertical";
+}
+
+export const Divider: FC<IDivideProps> = ({ orientation = "horizontal" }) => {
+	return (
+		<div
+			className={styles.divider}
+			data-orientation={orientation}
+		/>
+	);
+};
