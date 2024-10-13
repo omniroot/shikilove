2025c3298fbb29094772ff3b03c33f2cef951056
omniroot <omnirootofc@gmail.com>
2024-10-13 18:07:06 +0300

    Add TestPage

--- src/app/pages/TestPage/TestPage.tsx
+++ src/app/pages/TestPage/TestPage.tsx
@@ -1,6 +1,18 @@
-import { Box } from "@kuma-ui/core";
+import { useEffect } from "react";
 import styles from "./TestPage.module.scss";
+import axios from "axios";
 
 export const TestPage = () => {
-	return <Box className={styles.test}>123</Box>;
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const data = await axios.get("https://www.anilibria.tv/release/make-heroine-ga-oosugiru.html", {})
+			console.log(data)
+		}
+		fetchData()
+	}, [])
+
+	return <div className={styles.test}>
+		<iframe src="https://kodik.info/seria/1331355/e03d43d6288c6acdca1dfae4b6f6a882/720p?translations=false&min_age=16" allowFullScreen></iframe>
+	</div>;
 };
