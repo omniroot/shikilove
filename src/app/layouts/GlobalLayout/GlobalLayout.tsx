51c83872605ebba6e773acd09838b23efe069339
omniroot <omnirootofc@gmail.com>
2024-10-13 18:02:16 +0300

    Add rightSidebar to GlobalLayout

--- src/app/layouts/GlobalLayout/GlobalLayout.tsx
+++ src/app/layouts/GlobalLayout/GlobalLayout.tsx
@@ -4,8 +4,12 @@ import { Outlet } from "react-router-dom";
 import styles from "./GlobalLayout.module.scss";
 import { Sidebar } from "@widgets/Sidebar/Sidebar";
 import { LoginPage } from "@/app/pages/LoginPage/LoginPage";
+import { useGlobalStore } from "@/shared/store/store";
+import clsx from "clsx";
+import { AnimatePresence, motion } from 'framer-motion'
 
 export const GlobalLayout = () => {
+	const { isRightSidebarOpened, rightSidebarContent } = useGlobalStore(state => state)
 	const { loading, error, ...rest } = useFetchCurrentUser();
 	const { refreshTokens } = useAuthorization();
 
@@ -32,11 +36,18 @@ export const GlobalLayout = () => {
 	console.log("@ loading", loading);
 	console.log("@ rest", rest);
 	return (
-		<div className={styles.globallayout}>
+		<div className={styles.global_layout}>
 			<Sidebar />
 			<main className={styles.main}>
 				<Outlet />
 			</main>
+			<AnimatePresence mode="sync" >
+				{isRightSidebarOpened && (
+					<motion.div layout initial={{ width: 0 }} animate={{ width: "250px" }} exit={{ width: 0, }} transition={{ duration: 0.150, }} className={clsx(styles.right_sidebar, { [styles.opened]: isRightSidebarOpened })}>{rightSidebarContent}</motion.div>
+
+				)}
+
+			</AnimatePresence>
 		</div>
 	);
 };
