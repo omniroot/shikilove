3bcee26a1034fb988b8ffdc489c7922f372fa5b2
omniroot <omnirootofc@gmail.com>
2024-10-30 12:45:58 +0300

    Reduce website loading time

--- src/app/layouts/GlobalLayout/GlobalLayout.tsx
+++ src/app/layouts/GlobalLayout/GlobalLayout.tsx
@@ -13,13 +13,13 @@ import { FloatingSearchBar } from "@features/FloatingSearchBar/FloatingSearchBar
 import { Header } from "@widgets/Header/Header.tsx";
 
 export const GlobalLayout = () => {
-	const { currentUser, currentUserError, isCurrentUserLoading } = useAuthorization();
+	const { currentUserError } = useAuthorization();
 	const isMobile = useMediaQuery("only screen and (max-width: 768px)");
 	const isTablet = useMediaQuery("only screen and (min-width: 769px) and (max-width: 1024px)");
 	const isDesktop = useMediaQuery("only screen and (min-width: 1025px)");
 	const { isRightSidebarOpened, rightSidebarContent } = useGlobalStore((state) => state);
 
-	if (!currentUser && isCurrentUserLoading) return <div>Loading...</div>;
+	// if (!currentUser && isCurrentUserLoading) return <div>Loading...</div>;
 	if (currentUserError) {
 		console.log("Error while getting current user, try relogin", currentUserError);
 		return <LoginPage />;
