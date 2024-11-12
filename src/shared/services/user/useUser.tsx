9c3023b22568b07e24143ffcc2282dd908147a0b
omniroot <omnirootofc@gmail.com>
2024-11-12 23:48:02 +0300

    Add initial user service

---
+++ src/shared/services/user/useUser.tsx
@@ -0,0 +1,35 @@
+import { authApi } from "@/shared/services/auth/auth.api.ts";
+import { useQuery } from "@tanstack/react-query";
+
+export const useAuthorization = () => {
+	const {
+		isLoading: isCurrentUserLoading,
+		data: currentUser,
+		error: currentUserError,
+	} = useQuery({
+		queryKey: ["currentUser"],
+		queryFn: () => authApi.getCurrentUser(),
+	});
+
+	const {
+		isLoading: isFullCurrentUserLoading,
+		data: fullCurrentUser,
+		error: fullCurrentUserError,
+	} = useQuery({
+		queryKey: ["fullCurrentUser"],
+		queryFn: () => authApi.getFullCurrentUser(),
+	});
+
+	console.log("currentUser", currentUser);
+	console.log("fullCurrentUser", fullCurrentUser);
+
+	return {
+		isCurrentUserLoading,
+		currentUser,
+		currentUserError,
+
+		isFullCurrentUserLoading,
+		fullCurrentUser,
+		fullCurrentUserError,
+	};
+};
