4ab97292b92b17f6b78d56199047b9035c303ae4
omniroot <omnirootofc@gmail.com>
2024-11-13 00:04:16 +0300

    Refactoring useUser

--- src/shared/services/user/useUser.tsx
+++ src/shared/services/user/useUser.tsx
@@ -1,35 +1,21 @@
-import { authApi } from "@/shared/services/auth/auth.api.ts";
+import { userApi } from "@/shared/services/user/user.api.ts";
 import { useQuery } from "@tanstack/react-query";
 
-export const useAuthorization = () => {
+export const useUser = () => {
 	const {
 		isLoading: isCurrentUserLoading,
 		data: currentUser,
 		error: currentUserError,
 	} = useQuery({
 		queryKey: ["currentUser"],
-		queryFn: () => authApi.getCurrentUser(),
-	});
-
-	const {
-		isLoading: isFullCurrentUserLoading,
-		data: fullCurrentUser,
-		error: fullCurrentUserError,
-	} = useQuery({
-		queryKey: ["fullCurrentUser"],
-		queryFn: () => authApi.getFullCurrentUser(),
+		queryFn: () => userApi.getCurrentUser(),
 	});
 
 	console.log("currentUser", currentUser);
-	console.log("fullCurrentUser", fullCurrentUser);
 
 	return {
-		isCurrentUserLoading,
 		currentUser,
+		isCurrentUserLoading,
 		currentUserError,
-
-		isFullCurrentUserLoading,
-		fullCurrentUser,
-		fullCurrentUserError,
 	};
 };
