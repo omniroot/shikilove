6a9148ca38489bfb2dd83698a607707274fceded
omniroot <omnirootofc@gmail.com>
2024-11-13 00:03:43 +0300

    Refactoring auth service

--- src/shared/services/auth/auth.interface.ts
+++ src/shared/services/auth/auth.interface.ts
@@ -5,56 +5,6 @@ export interface ICurrentUser {
 	lastOnlineAt: string;
 }
 
-interface IFullStats {
-	anime: { id: number; grouped_id: string; name: string; size: number; type: string }[];
-	manga: { id: number; grouped_id: string; name: string; size: number; type: string }[];
-}
-
-interface IStats {
-	anime: { name: string; value: number }[];
-	manga: { name: string; value: number }[];
-}
-
-export interface IUser {
-	nickname: string;
-	avatar: string;
-	image: any;
-	last_online_at: string;
-	url: string;
-	name: any;
-	sex: string;
-	full_years: number;
-	last_online: string;
-	website: string;
-	location: any;
-	banned: boolean;
-	about: string;
-	about_html: string;
-	common_info: any[];
-	show_comments: boolean;
-	in_friends: boolean;
-	is_ignored: boolean;
-	stats: {
-		statuses: IFullStats;
-		full_statuses: IFullStats;
-		scores: IStats;
-		types: IStats;
-		ratings: IStats;
-		has_anime?: boolean;
-		has_manga?: boolean;
-		genres: unknown;
-		studios: unknown;
-		publishers: unknown;
-		activity: {
-			name: number[];
-			value: number;
-		}[];
-	};
-	style_id: number;
-}
-
-export type IFullCurrentUser = IUser;
-
 export interface IAuthResponse {
 	currentUser: ICurrentUser;
 }
