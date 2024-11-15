9e96f208218a7d4f7e0ed236703c5dfeb1bc4ec2
omniroot <omnirootofc@gmail.com>
2024-11-15 21:10:05 +0300

    Refactoring Global Layout, separate navigation logic to Navigation Layout

--- src/app/layouts/global/global.layout.tsx
+++ src/app/layouts/global/global.layout.tsx
@@ -1,17 +1,13 @@
 // import LoginPage from "@/app/pages/LoginPage/LoginPage.tsx";
-import { Outlet } from "react-router-dom";
-import styles from "./global.layout.module.scss";
-import { Header } from "@/shared/components/widgets/Header/Header.tsx";
-import { useMediaQuery } from "@uidotdev/usehooks";
-import { BottomNavigation } from "@ui/BottomNavigation/BottomNavigation.tsx";
 import { useUser } from "@/shared/services/user/useUser.tsx";
 import LoginPage from "@pages/login/login.page.tsx";
+import { Outlet } from "react-router-dom";
+import styles from "./global.layout.module.scss";
+import { NavigationLayout } from "@/app/layouts/navigation/navigation.layout.tsx";
 
 export const GlobalLayout = () => {
 	const { currentUserError } = useUser();
-	const isMobile = useMediaQuery("only screen and (max-width: 768px)");
-	// const isTablet = useMediaQuery("only screen and (min-width: 769px) and (max-width: 1024px)");
-	// const isDesktop = useMediaQuery("only screen and (min-width: 1025px)");
+
 	// const { isRightSidebarOpened, rightSidebarContent } = useGlobalStore((state) => state);
 
 	// if (!currentUser && isCurrentUserLoading) return <div>Loading...</div>;
@@ -23,30 +19,10 @@ export const GlobalLayout = () => {
 
 	return (
 		<div className={styles.global_layout}>
-			{/* {(isTablet || isDesktop) && <Sidebar />} */}
-			{/* {(isTablet || isDesktop) && <FloatingSearchButton />} */}
-			{isMobile && <Header />}
-			{/* <FloatingSearchBar /> */}
+			<NavigationLayout />
 			<main className={styles.main}>
 				<Outlet />
 			</main>
-			{isMobile && <BottomNavigation />}
-			{/* <AnimatePresence mode="sync">
-				{isRightSidebarOpened && (
-					<motion.div
-						layout
-						initial={{ width: 0 }}
-						animate={{ width: "250px" }}
-						exit={{ width: 0 }}
-						transition={{ duration: 0.15 }}
-						className={clsx(styles.right_sidebar, {
-							[styles.opened]: isRightSidebarOpened,
-						})}
-					>
-						{rightSidebarContent}
-					</motion.div>
-				)}
-			</AnimatePresence> */}
 		</div>
 	);
 };
