c9c0b73f85650afcf3a6f4900fff737ec17ab5c1
omniroot <omnirootofc@gmail.com>
2024-11-17 02:18:01 +0300

    Add getAnimeCardData util for get data from any types for anime card

---
+++ src/shared/utils/getAnimeCardData.ts
@@ -0,0 +1,49 @@
+import { ISearchAnime } from "@/shared/hooks/useSearchAnime.tsx";
+import { IAnime, ISimilarAnime } from "@/shared/services/anime/anime.interface.ts";
+import { IUserRate } from "@/shared/services/userRate/userRate.interface.ts";
+import { IAnimeCard } from "@/shared/types/anime_card.interface.ts";
+
+type IGetAnimeCardDataProps = IAnime | ISearchAnime | IUserRate | ISimilarAnime;
+export const getAnimeCardData = (anime: IGetAnimeCardDataProps): IAnimeCard => {
+	const animeCard: IAnimeCard | any = {};
+
+	if (anime._type === "IAnime") {
+		// console.log("IAnime", anime);
+
+		const _anime = anime as IAnime;
+		animeCard.id = _anime.id;
+		animeCard.poster = _anime.poster.mainUrl;
+		animeCard.name = _anime.name;
+		animeCard.russian = _anime.russian;
+		animeCard.episodes = _anime.episodes;
+		animeCard.score = _anime.score;
+		animeCard.kind = _anime.kind;
+		animeCard.airedOn = String(_anime.airedOn.year);
+		animeCard.userRate = {
+			status: _anime.userRate?.status,
+			episodes: _anime.userRate?.episodes,
+			score: _anime.userRate?.score,
+		};
+	}
+
+	if (anime._type === "ISearchAnime") {
+		// console.log("ISearchAnime", anime);
+
+		const _anime = anime as ISearchAnime;
+		animeCard.id = _anime.id;
+		animeCard.poster = _anime.poster.main2xUrl;
+		animeCard.name = _anime.name;
+		animeCard.russian = _anime.russian;
+		animeCard.episodes = _anime.episodes;
+		animeCard.score = _anime.score;
+		animeCard.kind = _anime.kind;
+		animeCard.airedOn = String(_anime.airedOn.year);
+		animeCard.userRate = {
+			status: _anime.userRate?.status,
+			episodes: _anime.userRate?.episodes,
+			score: _anime.userRate?.score,
+		};
+	}
+
+	return animeCard;
+};
