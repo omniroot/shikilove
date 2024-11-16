65d4308de5ed915152b0ecabb5d296364b43e01d
omniroot <omnirootofc@gmail.com>
2024-11-17 02:17:35 +0300

    Update useSearchAnime

--- src/shared/hooks/useSearchAnime.tsx
+++ src/shared/hooks/useSearchAnime.tsx
@@ -1,4 +1,5 @@
 import { graphql } from "@/shared/services/graphql.ts";
+import { IType } from "@/shared/types/_type.interface.ts";
 import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 
@@ -7,6 +8,17 @@ const GET_SEARCH_ANIME = `
 		animes(search: $name, limit: 15, kind: "!special") {
 			id
 			name
+			russian
+      episodes
+      episodesAired
+			score
+			kind
+			releasedOn {
+      	year
+      }
+			airedOn {
+				year
+			}
 			poster {
 				main2xUrl
 			}
@@ -21,9 +33,22 @@ const GET_SEARCH_ANIME = `
 	}
 `;
 
-interface IAnime {
+export interface ISearchAnime {
+	_type: IType;
 	id: string;
 	name: string;
+	russian: string;
+	episodes: number;
+	episodesAired: number;
+	score: number;
+	kind: string;
+	releasedOn: {
+		year: number;
+	};
+	airedOn: {
+		year: number;
+	};
+
 	poster: {
 		main2xUrl: string;
 	};
@@ -37,30 +62,37 @@ interface IAnime {
 }
 
 interface IResponse {
-	animes: IAnime[];
+	animes: ISearchAnime[];
 }
 
-export const useSearchAnime = () => {
-	const [searchAnimesQuery, setSearchAnimesQuery] = useState("");
+export const useSearchAnime = (defaultSearch: string) => {
+	const [searchAnimesQuery, setSearchAnimesQuery] = useState(defaultSearch);
 	const {
 		refetch: refetchSearchAnimes,
 		isFetching: searchAnimesIsLoading,
 		data: searchAnimes,
 		error: searchAnimesError,
-	} = useQuery<IAnime[]>({
+	} = useQuery<ISearchAnime[]>({
 		queryKey: ["searchAnime"],
 		enabled: false,
 		queryFn: async () => {
-			const { animes } = await graphql<IResponse>(GET_SEARCH_ANIME, { name: searchAnimesQuery });
+			let { animes } = await graphql<IResponse>(GET_SEARCH_ANIME, { name: searchAnimesQuery });
+			animes = animes.map((anime) => ({
+				...anime,
+				_type: "ISearchAnime",
+			}));
 			return animes;
 		},
 	});
+
+	console.log("Default search query", defaultSearch);
 
 	return {
 		refetchSearchAnimes,
 		searchAnimesIsLoading,
 		searchAnimes,
 		searchAnimesError,
+		searchAnimesQuery,
 		setSearchAnimesQuery,
 	};
 };
