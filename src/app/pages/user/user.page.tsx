036acb5f704c45370851b7ca7641bf88ea74d997
omniroot <omnirootofc@gmail.com>
2024-11-22 00:03:47 +0300

    Some

--- src/app/pages/user/user.page.tsx
+++ src/app/pages/user/user.page.tsx
@@ -1,7 +1,6 @@
-import { useUser } from "@/shared/services/user/hooks/useUser";
 import styles from "./user.page.module.scss";
 export const UserPage = () => {
-	const { currentUser } = useUser();
+	// const { currentUser } = useUser();
 
 	return <div className={styles.user_page}></div>;
 };
