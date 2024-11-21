4363f2c754aff5f1ed683abcacd56bb6466033ac
omniroot <omnirootofc@gmail.com>
2024-11-21 07:26:55 +0300

    Update DiscoveryPage, add ongoing list

--- src/app/pages/discovery/discovery.page.tsx
+++ src/app/pages/discovery/discovery.page.tsx
@@ -5,13 +5,19 @@ import {
 	RecentlyIcon,
 } from "@/shared/icons/index.tsx";
 import { useCurrentUser } from "@/shared/services/user/hooks/useCurrentUser.tsx";
+import { getButtonGroupElementById } from "@/shared/utils/getButtonGroupElement";
 import { ProfilePageSkeleton } from "@pages/profile/profile.page.skeleton.tsx";
 import { ButtonGroup, IButtonGroupElement } from "@ui/ButtonGroup/ButtonGroup.tsx";
-import { useDebounce } from "@uidotdev/usehooks";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 import styles from "./discovery.page.module.scss";
+import { useAnimeOngoings } from "@/shared/services/anime/hooks/useAnimeOngoing.tsx";
+import { AnimeList } from "@features/AnimeList/AnimeList.tsx";
+import { AnimeCard } from "@features/AnimeCard/AnimeCard.tsx";
+import { getAnimeCardData } from "@/shared/utils/getAnimeCardData.ts";
 
 const discoveryFilterButtonsList: IButtonGroupElement[] = [
+	{ id: "ongoing", title: "Ongoing", icon: <RecentlyIcon width={16} /> },
 	{ id: "recently", title: "Recently", icon: <RecentlyIcon width={16} /> },
 	{ id: "critique", title: "Critique", icon: <CritiquesIcon width={16} /> },
 	{ id: "collections", title: "Collections", icon: <CollectionsIcon width={16} /> },
@@ -20,12 +26,21 @@ const discoveryFilterButtonsList: IButtonGroupElement[] = [
 
 export const DiscoveryPage = () => {
 	const { currentUser } = useCurrentUser();
-	const [activeElement, setActiveElement] = useState(discoveryFilterButtonsList[0]);
-	const activeElementDebounced = useDebounce(activeElement, 80);
+	const [searchParams, setSearchParams] = useSearchParams();
+	const currentFilter = searchParams.get("filter") || "recently";
+	const [activeElement, setActiveElement] = useState(
+		getButtonGroupElementById(discoveryFilterButtonsList, currentFilter),
+	);
+	const { animeOngoings, isAnimeOngoingsLoading } = useAnimeOngoings();
 
 	const onElementClick = (nextElement: IButtonGroupElement) => {
+		setSearchParams({ filter: nextElement.id });
 		setActiveElement(nextElement);
 	};
+
+	useEffect(() => {
+		setActiveElement(getButtonGroupElementById(discoveryFilterButtonsList, currentFilter));
+	}, [searchParams]);
 
 	if (!currentUser) return <ProfilePageSkeleton />;
 	return (
@@ -33,9 +48,19 @@ export const DiscoveryPage = () => {
 			<ButtonGroup
 				className={styles.dicovery_filter_buttons}
 				elements={discoveryFilterButtonsList}
-				activeElement={activeElementDebounced}
+				activeElement={activeElement}
 				setActiveElement={onElementClick}
 			/>
+
+			{activeElement.id === "ongoing" && (
+				<AnimeList>
+					{!isAnimeOngoingsLoading &&
+						animeOngoings &&
+						animeOngoings.map((ongoing) => (
+							<AnimeCard key={ongoing.id} animeCard={getAnimeCardData(ongoing)} />
+						))}
+				</AnimeList>
+			)}
 		</div>
 	);
 };
