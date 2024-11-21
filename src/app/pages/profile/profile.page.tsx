69cce82cb57c3c25ddedffc989997a6fc3a306d9
omniroot <omnirootofc@gmail.com>
2024-11-21 07:26:30 +0300

    Formating

--- src/app/pages/profile/profile.page.tsx
+++ src/app/pages/profile/profile.page.tsx
@@ -1,8 +1,8 @@
+import { useCurrentUser } from "@/shared/services/user/hooks/useCurrentUser.tsx";
 import { AchievementsButton, ProfileInfoCard, ProfileUserRates } from "@pages/profile/_components";
+import { ProfilePageSkeleton } from "@pages/profile/profile.page.skeleton.tsx";
 import { FC, ReactNode } from "react";
 import styles from "./profile.page.module.scss";
-import { useCurrentUser } from "@/shared/services/user/hooks/useCurrentUser.tsx";
-import { ProfilePageSkeleton } from "@pages/profile/profile.page.skeleton.tsx";
 
 interface IProfilePageProps {
 	children?: ReactNode;
