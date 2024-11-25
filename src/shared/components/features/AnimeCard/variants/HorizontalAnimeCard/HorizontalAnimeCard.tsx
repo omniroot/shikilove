6497747ced8959694694ca0e0d2ba93cbe06acb2
omniroot <omnirootofc@gmail.com>
2024-11-25 13:48:33 +0300

    Refactoring AnimeCard's for uses unified props, like name, poster, episodes

--- src/shared/components/features/AnimeCard/variants/HorizontalAnimeCard/HorizontalAnimeCard.tsx
+++ src/shared/components/features/AnimeCard/variants/HorizontalAnimeCard/HorizontalAnimeCard.tsx
@@ -1,43 +1,55 @@
-import { StarIcon } from "@/shared/icons/index.tsx";
+import { RightArrowIcon } from "@/shared/icons/index.tsx";
 import { IAnimeCard } from "@/shared/types/anime_card.interface.ts";
 import { ImageView } from "@ui/ImageView/ImageView.tsx";
+import { motion } from "framer-motion";
 import { FC } from "react";
 import { Link } from "react-router-dom";
 import styles from "./HorizontalAnimeCard.module.scss";
-import { motion } from "framer-motion";
 
 interface IHorizontalAnimeCardProps {
-	animeCard: IAnimeCard;
+	anime: IAnimeCard;
 }
 
-export const HorizontalAnimeCard: FC<IHorizontalAnimeCardProps> = ({ animeCard }) => {
+export const HorizontalAnimeCard: FC<IHorizontalAnimeCardProps> = ({ anime }) => {
 	return (
 		<motion.div
-			className={styles.anime_card}
-			initial={{ opacity: 0.9, scale: 1, y: 30 }}
-			whileInView={{ opacity: 1, scale: 1, y: 0 }}
+			initial={{ opacity: 0.9, scale: 0.9 }}
+			whileInView={{ opacity: 1, scale: 1 }}
 			transition={{ duration: 0.25 }}
+			className={styles.anime_card}
 		>
-			<ImageView src={animeCard.poster} alt={animeCard.name} className={styles.anime_image} />
-			<Link to={`/animes/${animeCard.id}`} className={styles.info_container}>
-				<div className={styles.anime_title}>{animeCard.name}</div>
-				<div className={styles.subinfo_container}>
-					<span className={styles.user_status}>{animeCard.userRate?.status}</span>
-					{/* <RightArrowIcon /> */}
+			<ImageView src={anime.poster} alt="poster" className={styles.poster} />
+			<Link to={`/animes/${anime.id}`} className={styles.info}>
+				<span className={styles.title}>{anime.name}</span>
+				<div className={styles.line}>
+					{anime.userRate?.status}
+					<RightArrowIcon height={12} />
 					<span className={styles.user_episodes}>
-						{animeCard.userRate?.episodes} /{animeCard.episodes}
-					</span>
-					{/* <RightArrowIcon /> */}
-					<span>
-						{animeCard.score} <StarIcon />
+						{anime.userRate?.episodes} /{anime.episodes}
 					</span>
 				</div>
-				<div className={styles.bottominfo}>
-					<span>{animeCard.kind}</span>
-					<span>-</span>
-					<span>{animeCard.airedOn}</span>
-				</div>
+				{/* <div className={styles.bottominfo}>{anime.episodes}</div> */}
 			</Link>
 		</motion.div>
 	);
 };
+
+// export const HorizontalAnimeCard: FC<IHorizontalAnimeCardProps> = ({ animeCard }) => {
+// 	return (
+// 		<motion.div
+//
+// 		>
+// 			<ImageView src={animeCard.poster} alt={animeCard.name} className={styles.anime_image} />
+//
+// 				<div className={styles.anime_title}>{animeCard.name}</div>
+//
+// 				</div>
+// 				<div className={styles.bottominfo}>
+// 					<span>{animeCard.kind}</span>
+// 					<span>-</span>
+// 					<span>{animeCard.airedOn}</span>
+// 				</div>
+//
+// 		</motion.div>
+// 	);
+// };
