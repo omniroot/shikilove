bcec9d8869b42c76f2ba3ed7f9f343187d149932
omniroot <omnirootofc@gmail.com>
2024-11-27 22:54:22 +0300

    Wrap BottomSheet in createPortal

--- src/shared/components/ui/BottomSheet/BottomSheet.tsx
+++ src/shared/components/ui/BottomSheet/BottomSheet.tsx
@@ -1,8 +1,9 @@
-import { ReactNode, FC, MouseEvent } from "react";
-import styles from "./BottomSheet.module.scss";
-import clsx from "clsx";
-import { AnimatePresence, motion } from "framer-motion";
 import { Divider } from "@ui/Divider/Divider.tsx";
+import clsx from "clsx";
+import { motion } from "framer-motion";
+import { FC, MouseEvent, ReactNode } from "react";
+import styles from "./BottomSheet.module.scss";
+import { createPortal } from "react-dom";
 interface IBottomSheetProps {
 	children?: ReactNode;
 	className?: string;
@@ -15,38 +16,42 @@ export const BottomSheet: FC<IBottomSheetProps> = ({
 	title,
 	onOutsideClick = () => {},
 }) => {
-	const _class = clsx(styles.bottom_sheet, className);
-
 	const _onOutsideClick = () => {
 		onOutsideClick();
 	};
 
 	const _onBottomSheetClick = (event: MouseEvent<HTMLDivElement>) => {
-		event.preventDefault();
 		event.stopPropagation();
 	};
+	const _class = clsx(styles.bottom_sheet, className);
 
 	return (
-		<motion.div
-			className={styles.container}
-			initial={{ opacity: 0 }}
-			animate={{ opacity: 1 }}
-			exit={{ opacity: 0 }}
-			transition={{ duration: 0.2 }}
-			onClick={_onOutsideClick}
-		>
-			<motion.div
-				initial={{ y: 50 }}
-				animate={{ y: 0 }}
-				exit={{ y: 50 }}
-				transition={{ duration: 0.2 }}
-				className={_class}
-				onClick={_onBottomSheetClick}
-			>
-				<span className={styles.title}>{title}</span>
-				<Divider />
-				{children}
-			</motion.div>
-		</motion.div>
+		<>
+			{createPortal(
+				<motion.div
+					className={styles.container}
+					initial={{ opacity: 0 }}
+					animate={{ opacity: 1 }}
+					exit={{ opacity: 0 }}
+					transition={{ duration: 0.2 }}
+					onClick={_onOutsideClick}
+				>
+					<motion.div
+						initial={{ y: 50 }}
+						animate={{ y: 0 }}
+						exit={{ y: 50 }}
+						transition={{ duration: 0.2 }}
+						className={_class}
+						onClick={_onBottomSheetClick}
+					>
+						<span className={styles.title}>{title}</span>
+						<Divider />
+						{children}
+					</motion.div>
+				</motion.div>,
+
+				document.body,
+			)}
+		</>
 	);
 };
