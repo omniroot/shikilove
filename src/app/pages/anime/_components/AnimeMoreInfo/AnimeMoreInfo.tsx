62c196c110b9ff4e743aa6496cb3d75925f353f3
omniroot <omnirootofc@gmail.com>
2024-11-28 12:02:13 +0300

    Add animation in AnimeMoreInfo

--- src/app/pages/anime/_components/AnimeMoreInfo/AnimeMoreInfo.tsx
+++ src/app/pages/anime/_components/AnimeMoreInfo/AnimeMoreInfo.tsx
@@ -1,13 +1,15 @@
 import { IAnime } from "@/shared/services/anime/anime.interface.ts";
 import styles from "./AnimeMoreInfo.module.scss";
 import { FC } from "react";
+import { motion } from "framer-motion";
+
 interface IAnimeMoreInfoProps {
 	anime: IAnime | undefined;
 }
 export const AnimeMoreInfo: FC<IAnimeMoreInfoProps> = ({ anime }) => {
 	if (!anime) return "Anime more info loading...";
 	return (
-		<div className={styles.anime_more_info}>
+		<motion.div className={styles.anime_more_info} initial={{ scale: 0.9 }} animate={{ scale: 1 }}>
 			<div className={styles.element}>
 				<span className={styles.title}>Skikimiori</span>
 				<span className={styles.content}>{anime.score}</span>
@@ -20,6 +22,6 @@ export const AnimeMoreInfo: FC<IAnimeMoreInfoProps> = ({ anime }) => {
 				<span className={styles.title}>Duration</span>
 				<span className={styles.content}>{anime.duration}</span>
 			</div>
-		</div>
+		</motion.div>
 	);
 };
