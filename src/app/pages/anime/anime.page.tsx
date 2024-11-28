3d590abc8937c5b10c1ba900126f565adb6948a6
omniroot <omnirootofc@gmail.com>
2024-11-28 12:02:37 +0300

    Add AnimeScreenshots

--- src/app/pages/anime/anime.page.tsx
+++ src/app/pages/anime/anime.page.tsx
@@ -1,9 +1,10 @@
 import { useAnime } from "@/shared/services/anime/hooks/useAnime.tsx";
 import { AnimeInfo } from "@pages/anime/_components/AnimeInfo/AnimeInfo.tsx";
+import { AnimeMoreInfo } from "@pages/anime/_components/AnimeMoreInfo/AnimeMoreInfo.tsx";
+import { AnimeScreenshots } from "@pages/anime/_components/AnimeScreenshots/AnimeScreenshots.tsx";
 import { AnimeWatchContainer } from "@pages/anime/_components/AnimeWatchContainer/AnimeWatchContainer.tsx";
 import { useParams } from "react-router-dom";
 import styles from "./anime.page.module.scss";
-import { AnimeMoreInfo } from "@pages/anime/_components/AnimeMoreInfo/AnimeMoreInfo.tsx";
 
 export const AnimePage = () => {
 	const { animeId } = useParams();
@@ -14,6 +15,7 @@ export const AnimePage = () => {
 			<AnimeInfo anime={anime} />
 			<AnimeMoreInfo anime={anime} />
 			<AnimeWatchContainer anime={anime} />
+			<AnimeScreenshots screenshots={anime?.screenshots || []} />
 		</div>
 	);
 };
