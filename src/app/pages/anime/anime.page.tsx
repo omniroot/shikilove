3c475039a8e6414e33682c97da96995a82da2dfd
omniroot <omnirootofc@gmail.com>
2024-11-28 17:26:22 +0300

    Change tab title in AnimePage

--- src/app/pages/anime/anime.page.tsx
+++ src/app/pages/anime/anime.page.tsx
@@ -1,14 +1,24 @@
+import { useChangeTitle } from "@/shared/hooks/useChangeTitle.tsx";
 import { useAnime } from "@/shared/services/anime/hooks/useAnime.tsx";
 import { AnimeInfo } from "@pages/anime/_components/AnimeInfo/AnimeInfo.tsx";
 import { AnimeMoreInfo } from "@pages/anime/_components/AnimeMoreInfo/AnimeMoreInfo.tsx";
 import { AnimeScreenshots } from "@pages/anime/_components/AnimeScreenshots/AnimeScreenshots.tsx";
 import { AnimeWatchContainer } from "@pages/anime/_components/AnimeWatchContainer/AnimeWatchContainer.tsx";
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import styles from "./anime.page.module.scss";
 
 export const AnimePage = () => {
 	const { animeId } = useParams();
 	const { anime } = useAnime(animeId || "20");
+	const { changeTitle } = useChangeTitle();
+
+	useEffect(() => {
+		if (anime) {
+			changeTitle(anime.russian);
+		}
+		return () => changeTitle("");
+	}, [anime]);
 
 	return (
 		<div className={styles.anime_page}>
