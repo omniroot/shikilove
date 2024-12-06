b8e8d9583861ee2650c94efbd7528f7b836c99f7
omniroot <omnirootofc@gmail.com>
2024-12-06 21:43:06 +0300

    Add ReactQueryDevtool

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,6 +2,7 @@ import { router } from "@/app/router.tsx";
 import "@/app/styles/main.css";
 import { CONSTS } from "@/shared/consts/consts.ts";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
@@ -42,6 +43,7 @@ createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<QueryClientProvider client={client}>
 			<RouterProvider router={router} />
+			<ReactQueryDevtools initialIsOpen={false} />
 		</QueryClientProvider>
 	</StrictMode>,
 );
