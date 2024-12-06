bada967c036cd28f5d5a13a29869530e6fc20689
omniroot <omnirootofc@gmail.com>
2024-12-06 17:24:35 +0300

    Fix for not showing pages if data not loaded

--- src/app/layouts/global/global.layout.tsx
+++ src/app/layouts/global/global.layout.tsx
@@ -6,15 +6,19 @@ import LoginPage from "@pages/login/login.page.tsx";
 import { Outlet, ScrollRestoration } from "react-router-dom";
 import styles from "./global.layout.module.scss";
 import { useSettings } from "@/shared/store/settings.store.tsx";
+import { useUserRate } from "@/shared/services/userRate/useUserRate.tsx";
 
 export const GlobalLayout = () => {
 	const { currentUserError } = useCurrentUser();
+	const { userRates } = useUserRate();
 	useSettings();
 
 	if (currentUserError) {
 		console.log("Error while getting current user, try relogin", currentUserError);
 		return <LoginPage />;
 	}
+
+	if (!userRates?.pages || currentUserError) return null;
 
 	return (
 		<div className={styles.global_layout}>
