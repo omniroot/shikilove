5f56a529d88b53645d383c614369024b4161d84e
omniroot <omnirootofc@gmail.com>
2024-12-06 21:43:44 +0300

    Fix router, now ProfilePage and AnimePage in main bundle

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -4,15 +4,17 @@ import { createBrowserRouter, createRoutesFromElements, Route } from "react-rout
 import { GlobalLayout } from "@/app/layouts/global/global.layout";
 import { ProfilePageSkeleton } from "@pages/profile/profile.page.skeleton.tsx";
 import { HomePage } from "@pages/home/home.page.tsx";
+import AnimePage from "@pages/anime/anime.page.tsx";
+import ProfilePage from "@pages/profile/profile.page.tsx";
 const LoginPage = React.lazy(() => import("@/app/pages/login/login.page.tsx"));
-const AnimePage = React.lazy(() => import("@/app/pages/anime/anime.page.tsx"));
+// const AnimePage = React.lazy(() => import("@/app/pages/anime/anime.page.tsx"));
 const ScreenshotsPage = React.lazy(
 	() => import("@/app/pages/anime/_pages/screenshots/screenshots.page.tsx"),
 );
 const SimilarsPage = React.lazy(
 	() => import("@/app/pages/anime/_pages/similars/similars.page.tsx"),
 );
-const ProfilePage = React.lazy(() => import("@/app/pages/profile/profile.page.tsx"));
+// const ProfilePage = React.lazy(() => import("@/app/pages/profile/profile.page.tsx"));
 const ProfileEditPage = React.lazy(
 	() => import("@pages/profile/_pages/profile_edit/profile_edit.page.tsx"),
 );
