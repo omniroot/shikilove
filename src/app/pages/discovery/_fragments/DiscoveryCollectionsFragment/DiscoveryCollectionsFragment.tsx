2767d15c0e6ba3d967055d2750fc8f32277aebea
omniroot <omnirootofc@gmail.com>
2024-12-08 19:52:48 +0300

    Fix DiscoveryCollectionsFragment

--- src/app/pages/discovery/_fragments/DiscoveryCollectionsFragment/DiscoveryCollectionsFragment.tsx
+++ src/app/pages/discovery/_fragments/DiscoveryCollectionsFragment/DiscoveryCollectionsFragment.tsx
@@ -8,16 +8,19 @@ export const DiscoveryCollectionsFragment = () => {
 	if (!collections || isCollectionsLoading) return "Loading...";
 	return (
 		<AnimeList scroll="none">
-			{collections.map((collection) => (
-				<AnimeCard
-					key={collection.id}
-					variant="horizontal"
-					anime={{
-						id: String(collection.id),
-						name: collection.topic_title,
-					}}
-				/>
-			))}
+			{collections.map((collection) => {
+				return (
+					<AnimeCard
+						key={collection.id}
+						variant="horizontal"
+						anime={{
+							id: String(collection.id),
+							name: collection.topic_title,
+						}}
+						testSlot={<a href={`https://shikimori.me${collection.forum.url}`}>Read on Shikimori</a>}
+					/>
+				);
+			})}
 		</AnimeList>
 	);
 };
