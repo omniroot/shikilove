007080e134ae41df2c92162aa528c631aa97943d
omniroot <omnirootofc@gmail.com>
2024-12-15 14:33:03 +0300

    Rewrite Portal component

--- src/shared/components/ui/Portal/Portal.tsx
+++ src/shared/components/ui/Portal/Portal.tsx
@@ -1,19 +1,22 @@
-import { motion } from "motion/react";
+import { AnimatePresence, motion } from "motion/react";
 import { FC, ReactNode } from "react";
 import { createPortal } from "react-dom";
 
 interface IPortalProps {
 	children: ReactNode;
+	show: boolean;
+	zIndex?: number;
 }
-export const Portal: FC<IPortalProps> = ({ children }) => {
-	return (
-		<>
-			{createPortal(
-				<motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+
+export const Portal: FC<IPortalProps> = ({ children, show, zIndex = 10000 }) => {
+	const element = (
+		<AnimatePresence>
+			{show && (
+				<motion.div initial={{ opacity: 0, zIndex }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
 					{children}
-				</motion.div>,
-				document.body,
+				</motion.div>
 			)}
-		</>
+		</AnimatePresence>
 	);
+	return <>{createPortal(element, document.body)}</>;
 };
