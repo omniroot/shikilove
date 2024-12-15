aab748b64647661991e8dc48faecbacf3321722b
omniroot <omnirootofc@gmail.com>
2024-12-15 15:40:43 +0300

    Fix login not worki

--- src/app/layouts/global/global.layout.tsx
+++ src/app/layouts/global/global.layout.tsx
@@ -1,25 +1,26 @@
 // import LoginPage from "@/app/pages/LoginPage/LoginPage.tsx";
 import { NavigationLayout } from "@/app/layouts/navigation/navigation.layout.tsx";
 import { SearchLayout } from "@/app/layouts/search/search.layout.tsx";
+import { useScrollSave } from "@/shared/hooks/useScrollSave.tsx";
 import { useCurrentUser } from "@/shared/services/user/hooks/useCurrentUser.tsx";
 import { useUserRate } from "@/shared/services/userRate/useUserRate.tsx";
 import { useSettings } from "@/shared/store/settings.store.tsx";
-import { Outlet, redirect } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 import styles from "./global.layout.module.scss";
-import { useScrollSave } from "@/shared/hooks/useScrollSave.tsx";
 
 export const GlobalLayout = () => {
 	const { currentUserError } = useCurrentUser();
-	const { userRates } = useUserRate();
+	const navigate = useNavigate();
 	useSettings();
 	useScrollSave();
 
-	if (currentUserError) {
-		console.log("Error while getting current user, try relogin", currentUserError);
-		redirect("/login");
-	}
-
-	if (!userRates?.pages || currentUserError) return null;
+	useEffect(() => {
+		if (currentUserError) {
+			console.log("Error while getting current user, try relogin", currentUserError);
+			navigate("/login", { replace: true });
+		}
+	}, []);
 
 	return (
 		<div className={styles.global_layout}>
