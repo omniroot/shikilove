8023a18b341fbc91a5c04892126ac4ef4d7c1357
omniroot <omnirootofc@gmail.com>
2024-12-15 14:58:40 +0300

    Update Settings page design

--- src/app/pages/settings/settings.page.tsx
+++ src/app/pages/settings/settings.page.tsx
@@ -1,8 +1,9 @@
 import { THEMES, useSettings } from "@/shared/store/settings.store.tsx";
 import { Button } from "@ui/Button/Button.tsx";
 import { HeadingSection } from "@ui/HeadingSection/HeadingSection.tsx";
-import { Select } from "@ui/Select/Select.tsx";
+import { Select, SelectContent, SelectItem } from "@ui/Select/Select.tsx";
 import styles from "./settings.page.module.scss";
+import { Divider } from "@ui/Divider/Divider.tsx";
 
 export const SettingsPage = () => {
 	const { theme, setTheme } = useSettings();
@@ -17,15 +18,43 @@ export const SettingsPage = () => {
 		<div className={styles.settings_page}>
 			<span className={styles.settings_heading}>Settings</span>
 			<Select
-				elements={themeSelectElements}
-				value={theme}
-				onChange={(newTheme) => setTheme(newTheme as typeof theme)}
-			/>
+				defaultValue={{ value: theme, label: theme }}
+				onActiveChange={(newTheme) => setTheme(newTheme as typeof theme)}
+				positionX="right"
+			>
+				<SelectContent>
+					{themeSelectElements.map((element) => {
+						return (
+							<SelectItem key={element.value} value={element.value}>
+								{element.label}
+							</SelectItem>
+						);
+					})}
+				</SelectContent>
+			</Select>
 			<HeadingSection title="Preview theme">
+				<div className={styles.preview_colors}>
+					<section>
+						<span>Background:</span>
+						<Button circle className={styles.preview_primary}></Button>
+						<Button circle className={styles.preview_secondary}></Button>
+						<Button circle className={styles.preview_ternary}></Button>
+
+						<Divider orientation="vertical" className={styles.divider} />
+					</section>
+					<section>
+						<span>Text:</span>
+						<Button circle className={styles.preview_text}></Button>
+						<Button circle className={styles.preview_subtext}></Button>
+						<Button circle className={styles.preview_alttext}></Button>
+					</section>
+				</div>
+				<Divider />
 				<Button variant="primary">Primary Button</Button>
 				<Button variant="secondary">Secondary Button</Button>
-				<Button variant="ternary">Ternary Button</Button>
-				<Select elements={[{ label: "test", value: "test" }]} value="test" onChange={() => {}} />
+				<Button variant="outline">Outline Button</Button>
+				<Button variant="gradient">Gradient Button</Button>
+				<Button variant="ghost">Ghost Button</Button>
 			</HeadingSection>
 		</div>
 	);
