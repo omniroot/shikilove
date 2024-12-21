4a8bed7eaf8abde5ba0b3800c74f9637ee1e5228
omniroot <omnirootofc@gmail.com>
2024-12-21 10:14:27 +0300

    Update AnimePlayer

--- src/shared/components/features/AnimePlayer/AnimePlayer.tsx
+++ src/shared/components/features/AnimePlayer/AnimePlayer.tsx
@@ -1,98 +1,323 @@
-// import clsx from "clsx";
-// import styles from "./AnimePlayer.module.scss";
+import clsx from "clsx";
+import { Maximize2Icon, PauseIcon, PlayIcon } from "lucide-react";
+import React, { FC, forwardRef, useEffect, useRef, useState } from "react";
+import styles from "./AnimePlayer.module.scss";
+import { Button } from "@ui/Button/Button";
+import { AnimatePresence, motion } from "motion/react";
 
-// import { Button } from "@ui/Button/Button";
-// import { useToggle } from "@uidotdev/usehooks";
-// import { PauseIcon, PlayIcon } from "lucide-react";
-// import React, { forwardRef, useRef, useState } from "react";
+const formatTime = (seconds: number) => {
+	const totalSeconds = Math.floor(seconds);
+	const minutes = Math.floor(totalSeconds / 60);
+	const hours = Math.floor(minutes / 60);
+	const displayMinutes = minutes % 60;
+	const displaySeconds = totalSeconds % 60;
 
-// interface IAnimePlayerProps
-// 	extends React.DetailedHTMLProps<React.VideoHTMLAttributes<HTMLVideoElement>, HTMLVideoElement> {}
+	if (hours > 0) {
+		return `${hours}:${displayMinutes < 10 ? "0" : ""}${displayMinutes}:${
+			displaySeconds < 10 ? "0" : ""
+		}${displaySeconds}`;
+	} else {
+		return `${displayMinutes}:${displaySeconds < 10 ? "0" : ""}${displaySeconds}`;
+	}
+};
 
-// type IPlayerStatus = "playing" | "stoped";
-// export const AnimePlayer = forwardRef<HTMLVideoElement, IAnimePlayerProps>(
-// 	({ className, ...rest }, ref) => {
-// 		const [overlayOpen, toggleOverlayOpen] = useToggle(false);
-// 		const [playerStatus, setPlayerStatus] = useState<IPlayerStatus>("stoped");
-// 		const [duration, setDuration] = useState(0);
-// 		const [currentTime, setCurrentTime] = useState<number>(0);
-// 		const playerRef = useRef<HTMLVideoElement>(null);
+interface IAnimePlayerProps
+	extends React.DetailedHTMLProps<React.VideoHTMLAttributes<HTMLVideoElement>, HTMLVideoElement> {
+	title?: string;
+}
 
-// 		const togglePlayerStatus = () => {
-// 			const nextStatus = playerStatus === "playing" ? "stoped" : "playing";
+type IPlayerStatus = "playing" | "stoped";
+export const AnimePlayer: FC<IAnimePlayerProps> = ({ className, title, ...rest }) => {
+	const [isFullscreen, setIsFullscreen] = useState(false);
+	const [isMouseHide, setIsMouseHide] = useState(false);
+	const [overlayOpen, setOverlayOpen] = useState(false);
+	const [playerStatus, setPlayerStatus] = useState<IPlayerStatus>("stoped");
+	const [duration, setDuration] = useState(0);
+	const [currentTime, setCurrentTime] = useState<number>(0);
+	const playerRef = useRef<HTMLVideoElement>(null);
+	const containerRef = useRef<HTMLDivElement>(null);
+	const timelineRef = useRef<HTMLDivElement>(null);
 
-// 			if (nextStatus === "playing") {
-// 				playerRef.current?.play();
-// 			} else {
-// 				playerRef.current?.pause();
-// 			}
-// 			setPlayerStatus(nextStatus);
-// 		};
+	const togglePlayerStatus = () => {
+		const nextStatus = playerStatus === "playing" ? "stoped" : "playing";
 
-// 		const onPlayButtonClick = () => {
-// 			togglePlayerStatus();
-// 		};
+		if (nextStatus === "playing") {
+			playerRef.current?.play();
+		} else {
+			playerRef.current?.pause();
+		}
+		setPlayerStatus(nextStatus);
+	};
 
-// 		const onVideoLoaded = () => {
-// 			if (playerRef.current) setDuration(playerRef.current?.duration);
-// 		};
+	const toggleFullscreen = () => {
+		console.log({ isFullscreen, docu: document.fullscreenElement });
 
-// 		const handleSeek = (event: React.ChangeEvent<HTMLInputElement>) => {
-// 			const newValue = Number(event.target.value);
-// 			if (playerRef.current) {
-// 				playerRef.current.currentTime = newValue;
-// 			}
-// 			setCurrentTime(newValue);
-// 		};
+		if (!isFullscreen) {
+			const element = containerRef.current ?? null;
+			if (!element) return;
+			console.log("enable fullscreen");
+			if (element.requestFullscreen) {
+				element.requestFullscreen();
+				// @ts-expect-error browsers support...
+			} else if (element.mozRequestFullScreen) {
+				// Firefox
+				// @ts-expect-error browsers support...
 
-// 		const onOverlayClick = (event: React.MouseEvent<HTMLDivElement>) => {
-// 			event.stopPropagation();
-// 			// toggleOverlayOpen();
-// 		};
+				element.mozRequestFullScreen();
+				// @ts-expect-error browsers support...
+			} else if (element.webkitRequestFullscreen) {
+				// Chrome, Safari и Opera
+				// @ts-expect-error browsers support...
 
-// 		const onContainerClick = () => {
-// 			toggleOverlayOpen();
-// 		};
+				element.webkitRequestFullscreen();
+				// @ts-expect-error browsers support...
+			} else if (element.msRequestFullscreen) {
+				// IE/Edge
+				// @ts-expect-error browsers support...
+				element.msRequestFullscreen();
+			}
 
-// 		const _class = clsx(styles.player, className);
-// 		return (
-// 			<div className={styles.container} onClick={onContainerClick}>
-// 				<video
-// 					{...rest}
-// 					ref={playerRef}
-// 					className={_class}
-// 					onLoadedMetadata={onVideoLoaded}
-// 					controls
-// 				/>
-// 				{overlayOpen && (
-// 					<div className={styles.overlay} onClick={onOverlayClick}>
-// 						<div className={styles.top}>Video name</div>
-// 						<div className={styles.center}>
-// 							<Button circle onClick={onPlayButtonClick}>
-// 								{playerStatus === "playing" ? <PauseIcon /> : <PlayIcon />}
-// 							</Button>
-// 							<Button onClick={() => toggleOverlayOpen()}>Hide</Button>
-// 						</div>
-// 						<div className={styles.bottom}>
-// 							<div className={styles.timeline_container}>
-// 								<div className={styles.timeline} />
-// 								<div
-// 									className={styles.position}
-// 									style={{ left: (playerRef.current?.currentTime / 60) % 60 }}
-// 								/>
-// 							</div>
-// 							{/* <input
-// 								type="range"
-// 								min="0"
-// 								max={duration}
-// 								value={currentTime}
-// 								onChange={handleSeek}
-// 								className={styles.seekbar}
-// 							/> */}
-// 						</div>
-// 					</div>
-// 				)}
-// 			</div>
-// 		);
-// 	},
-// );
+			setIsFullscreen(true);
+		} else {
+			console.log("disable fullscreen");
+
+			if (document.exitFullscreen) {
+				document.exitFullscreen();
+				// @ts-expect-error browsers support...
+			} else if (document.mozCancelFullScreen) {
+				// Firefox
+				// @ts-expect-error browsers support...
+
+				document.mozCancelFullScreen();
+				// @ts-expect-error browsers support...
+			} else if (document.webkitExitFullscreen) {
+				// Chrome, Safari и Opera
+				// @ts-expect-error browsers support...
+
+				document.webkitExitFullscreen();
+				// @ts-expect-error browsers support...
+			} else if (document.msExitFullscreen) {
+				// IE/Edge
+				// @ts-expect-error browsers support...
+				document.msExitFullscreen();
+			}
+			setIsFullscreen(false);
+		}
+		// setIsFullscreen(!);
+		// if (document.fullscreenEnabled)
+		// console.log(!!document.fullscreenElement);
+	};
+
+	const onPlayButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+		event.stopPropagation();
+		togglePlayerStatus();
+	};
+
+	const onFullscreenButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+		event.stopPropagation();
+		toggleFullscreen();
+	};
+
+	const onVideoLoaded = () => {
+		if (playerRef.current) {
+			playerRef.current.currentTime = 150;
+			setDuration(playerRef.current?.duration);
+		}
+	};
+
+	const onVideoPlaying = (event: React.SyntheticEvent<HTMLVideoElement, Event>) => {
+		setCurrentTime(event.currentTarget.currentTime);
+	};
+
+	const onTimelineClick = (event: React.MouseEvent<HTMLDivElement>) => {
+		event.stopPropagation();
+		const rect = event.currentTarget.getBoundingClientRect();
+		const offsetX = event.clientX - rect.left; // Позиция клика относительно таймлайна
+		const newTime = (offsetX / rect.width) * duration; // Вычисляем новое время
+		if (playerRef.current) {
+			playerRef.current.currentTime = newTime;
+		}
+		setCurrentTime(newTime);
+	};
+
+	const onVideoClick = (event: React.MouseEvent<HTMLVideoElement>) => {
+		event.stopPropagation();
+		event.preventDefault();
+
+		// setOverlayOpen((prev) => !prev);
+	};
+
+	const onOverlayClick = (event: React.MouseEvent<HTMLDivElement>) => {
+		event.stopPropagation();
+		event.preventDefault();
+
+		setOverlayOpen((prev) => !prev);
+	};
+
+	const onContainerClick = () => {
+		// setOverlayOpen((prev) => !prev);
+	};
+
+	useEffect(() => {
+		console.log("mouse handler update");
+
+		let timeout: any;
+
+		let mouseHandler = (event: globalThis.MouseEvent) => {
+			if (isMouseHide) {
+				setIsMouseHide(false);
+				document.body.style.cursor = "auto";
+			}
+
+			if (timeout) clearTimeout(timeout);
+			// console.log(event);
+
+			timeout = setTimeout(() => {
+				document.body.style.cursor = "none";
+
+				setIsMouseHide(true);
+				setOverlayOpen(false);
+			}, 1000);
+		};
+
+		// Устанавливаем новый таймер на 3 секунды
+
+		if (isFullscreen) {
+			document.addEventListener("mousemove", mouseHandler);
+		}
+
+		return () => {
+			if (timeout) clearTimeout(timeout);
+			document.removeEventListener("mousemove", mouseHandler);
+		};
+	}, [isFullscreen, overlayOpen]);
+
+	useEffect(() => {
+		const handleMouseEnter = (event: globalThis.MouseEvent) => {
+			console.log("mouse enter");
+
+			// if (!isMouseHide) {
+			setOverlayOpen(true);
+			// }
+		};
+		const handleMouseLeave = () => {
+			console.log("mouse leave");
+
+			setOverlayOpen(false);
+		};
+
+		const handleFullscreenChange = () => {
+			console.log("handle fullscreen");
+
+			if (!document.fullscreenElement) {
+				setIsFullscreen(false);
+			}
+		};
+
+		if (containerRef.current && playerRef.current) {
+			containerRef.current.addEventListener("mouseenter", handleMouseEnter);
+			containerRef.current.addEventListener("mouseleave", handleMouseLeave);
+			// containerRef.current.addEventListener("fullscreenchange", handleFullscreenChange);
+		}
+		return () => {
+			if (containerRef.current && playerRef.current) {
+				containerRef.current.removeEventListener("mouseenter", handleMouseEnter);
+				containerRef.current.removeEventListener("mouseleave", handleMouseLeave);
+				// containerRef.current.removeEventListener("fullscreenchange", handleFullscreenChange);
+			}
+		};
+	}, []);
+
+	const onDragStart = (e) => {
+		e.preventDefault();
+		document.addEventListener("mousemove", onDrag);
+		document.addEventListener("mouseup", onDragEnd);
+		document.addEventListener("touchmove", onDrag, { passive: false });
+		document.addEventListener("touchend", onDragEnd);
+	};
+
+	const onDrag = (e) => {
+		if (timelineRef.current) {
+			const rect = timelineRef.current.getBoundingClientRect();
+			const offsetX = e.clientX ? e.clientX - rect.left : e.touches[0].clientX - rect.left;
+			const newTime = Math.max(0, Math.min((offsetX / rect.width) * duration, duration));
+			if (playerRef.current) {
+				playerRef.current.currentTime = newTime;
+			}
+			setCurrentTime(newTime);
+		}
+	};
+
+	const onDragEnd = () => {
+		document.removeEventListener("mousemove", onDrag);
+		document.removeEventListener("mouseup", onDragEnd);
+		document.removeEventListener("touchmove", onDrag);
+		document.removeEventListener("touchend", onDragEnd);
+	};
+
+	const _class = clsx(styles.player, className, {
+		// [styles.fullscreen]: isFullscreen,
+	});
+	return (
+		<div className={styles.container} onClick={onContainerClick} ref={containerRef}>
+			<video
+				ref={playerRef}
+				className={_class}
+				onLoadedMetadata={onVideoLoaded}
+				onClick={onVideoClick}
+				onTimeUpdate={onVideoPlaying}
+				{...rest}
+			/>
+			<AnimatePresence>
+				{overlayOpen && !isMouseHide && (
+					<motion.div
+						className={styles.overlay}
+						onClick={onOverlayClick}
+						initial={{ opacity: 0 }}
+						animate={{ opacity: 1 }}
+						exit={{ opacity: 0 }}
+					>
+						<div className={styles.top}>{title}</div>
+						<div className={styles.center}>
+							<Button circle onClick={onPlayButtonClick}>
+								{playerStatus === "playing" ? <PauseIcon /> : <PlayIcon />}
+							</Button>
+							{/* <span>{isMouseHide ? "true" : "false"}</span> */}
+						</div>
+						<div className={styles.bottom}>
+							<div className={styles.timeline_container}>
+								<div className={styles.timeline} onClick={onTimelineClick} ref={timelineRef}>
+									<div
+										className={styles.position}
+										onMouseDown={onDragStart}
+										onTouchStart={onDragStart}
+										// onClick={onTimelineClick}
+										style={{ width: `${(currentTime / duration) * 100}%` }}
+									/>
+								</div>
+							</div>
+							<div className={styles.controls}>
+								{formatTime(currentTime)} / {formatTime(duration)}
+								<Button
+									circle
+									onClick={onFullscreenButtonClick}
+									className={styles.fullscreen_button}
+								>
+									<Maximize2Icon />
+								</Button>
+							</div>
+							{/* <input
+								type="range"
+								min="0"
+								max={duration}
+								value={currentTime}
+								onChange={handleSeek}
+								className={styles.seekbar}
+							/> */}
+						</div>
+					</motion.div>
+				)}
+			</AnimatePresence>
+		</div>
+	);
+};
