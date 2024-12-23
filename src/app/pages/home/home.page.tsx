594d7f0c578a60032538f080b652e8f97822ceaf
omniroot <omnirootofc@gmail.com>
2024-12-23 12:16:37 +0300

    Some fixes

--- src/app/pages/home/home.page.tsx
+++ src/app/pages/home/home.page.tsx
@@ -1,9 +1,7 @@
-// @ts-nocheck
 import { PlusIcon } from "@/shared/icons/index.tsx";
 import { Button } from "@ui/Button/Button.tsx";
 import styles from "./home.page.module.scss";
 import { AnimePlayer } from "@features/AnimePlayer/AnimePlayer";
-import { div } from "motion/react-client";
 
 export const HomePage = () => {
 	return (
