c603e9904f7ca1795c3acce5cee2ea9932265ec4
omniroot <omnirootofc@gmail.com>
2024-12-30 18:22:48 +0300

    Use test video in home page

--- src/app/pages/home/home.page.tsx
+++ src/app/pages/home/home.page.tsx
@@ -2,19 +2,25 @@ import { PlusIcon } from "@/shared/icons/index.tsx";
 import { Button } from "@ui/Button/Button.tsx";
 import styles from "./home.page.module.scss";
 import { AnimePlayer } from "@features/AnimePlayer/AnimePlayer";
+import { useLocalStorage } from "@uidotdev/usehooks";
+import { useEffect } from "react";
 
 export const HomePage = () => {
+	const [test, setTest] = useLocalStorage<string>("test");
+
+	const link =
+		"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4";
+
 	return (
 		<div className={styles.home_page}>
 			<span>No widgets in Home Page</span>
+			{test}
+			<Button onClick={() => setTest(String(Math.random()))}>change test variable</Button>
 			<Button variant="outline">
 				<PlusIcon />
 				Add widget
 			</Button>
-			<AnimePlayer
-				title="Необъятный океан"
-				src="https://video1.anilib.me/.%D0%B0s//uploads/converted_videos/anime/13850/players/65742/e5dc327f-0c86-41ee-a2bd-b934da8d8c33_2160.mp4"
-			/>
+			<AnimePlayer title="Chrome cast test" src={link} />
 		</div>
 	);
 };
