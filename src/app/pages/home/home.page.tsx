311c20dcb279a6a32493dc97b7080ae4688da6c5
omniroot <omnirootofc@gmail.com>
2025-01-03 01:21:56 +0300

    Add some debug shit in home

--- src/app/pages/home/home.page.tsx
+++ src/app/pages/home/home.page.tsx
@@ -1,26 +1,33 @@
 import { PlusIcon } from "@/shared/icons/index.tsx";
 import { Button } from "@ui/Button/Button.tsx";
-import styles from "./home.page.module.scss";
-import { AnimePlayer } from "@features/AnimePlayer/AnimePlayer";
-import { useLocalStorage } from "@uidotdev/usehooks";
 import { useEffect } from "react";
+import styles from "./home.page.module.scss";
 
 export const HomePage = () => {
-	const [test, setTest] = useLocalStorage<string>("test");
-
 	const link =
 		"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4";
+
+	const onButtonClick = () => {
+		// addABear();
+		// setTest(Math.random());
+	};
+
+	useEffect(() => {
+		window.addEventListener("storage", (event) => {
+			console.log(event);
+		});
+	}, []);
 
 	return (
 		<div className={styles.home_page}>
 			<span>No widgets in Home Page</span>
-			{test}
-			<Button onClick={() => setTest(String(Math.random()))}>change test variable</Button>
+			{/* {bears} */}
+			<Button onClick={onButtonClick}>change test variable</Button>
 			<Button variant="outline">
 				<PlusIcon />
 				Add widget
 			</Button>
-			<AnimePlayer title="Chrome cast test" src={link} />
+			{/* <AnimePlayer title="Chrome cast test" src={link} /> */}
 		</div>
 	);
 };
