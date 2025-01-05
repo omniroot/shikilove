5a821051fc849655f13c232020042cd620134598
omniroot <omnirootofc@gmail.com>
2025-01-05 09:42:14 +0300

    Fix no hidding inidicator in ModernBottomNavigation

--- src/shared/components/widgets/ModernBottomNavigation/ModernBottomNavigation.tsx
+++ src/shared/components/widgets/ModernBottomNavigation/ModernBottomNavigation.tsx
@@ -28,102 +28,105 @@ export const ModernBottomNavigation = () => {
 			activeIndicatorEndRef
 		) {
 			const activeItem = document.getElementById("active_bn") as HTMLLinkElement;
-			if (activeItem) {
-				const bottomNavgationWidth = bottomNavigationRef.current.offsetWidth;
-
-				const previousItemLeftOffset = prevActiveItem?.offsetLeft ?? 4;
-				const previousItemRightOffset =
-					bottomNavgationWidth - (prevActiveItem?.offsetLeft || 0) || 4;
-
-				const activeItemLeftOffset = activeItem.offsetLeft;
-				const activeItemRightOffset = bottomNavgationWidth - (activeItem.offsetLeft || 0);
-
-				const padding = Number(bottomNavigationRef.current.style.padding);
-
-				// console.log({
-				// 	activeItemLink: activeItem.href,
-				// 	bottomNavgationLeftOffset,
-				// 	bottomNavgationRightOffset,
-				// 	previousItemLeftOffset,
-				// 	previousItemRightOffset,
-				// 	activeItemLeftOffset,
-				// 	activeItemRightOffset,
-				// });
-
-				const direction =
-					activeItem.offsetLeft > activeIndicatorRef.current.offsetLeft ? "right" : "left";
-
-				// move active indicator (don`t touch it)
-				activeIndicatorRef.current.style.left = activeItem.offsetLeft + "px";
-
-				// initial
-				activeIndicatorStartRef.current.style.left = "unset";
-				activeIndicatorStartRef.current.style.right = "unset";
-				activeIndicatorEndRef.current.style.left = "unset";
-				activeIndicatorEndRef.current.style.right = "unset";
-
+			if (!activeItem) {
+				activeIndicatorRef.current.style.visibility = "hidden";
 				activeIndicatorStartRef.current.style.visibility = "hidden";
-				activeIndicatorStartRef.current.style.width = 50 + "px";
 				activeIndicatorEndRef.current.style.visibility = "hidden";
-				activeIndicatorEndRef.current.style.width = 50 + "px";
-				activeIndicatorEndRef.current.style.transition = "none";
-
-				if (direction === "right") {
-					activeIndicatorStartRef.current.style.visibility = "visible";
-
-					activeIndicatorStartRef.current.style.left = previousItemLeftOffset + "px";
-					activeIndicatorStartRef.current.style.width =
-						activeItemLeftOffset - previousItemLeftOffset - padding + 50 + "px";
-
-					setTimeout(() => {
-						if (!activeIndicatorStartRef.current || !activeIndicatorEndRef.current) return;
-
-						console.log(activeItem.offsetLeft);
-						activeIndicatorStartRef.current.style.visibility = "hidden";
-						activeIndicatorEndRef.current.style.visibility = "visible";
-
-						activeIndicatorEndRef.current.style.width =
-							previousItemRightOffset - activeItemRightOffset + 50 + "px";
-						activeIndicatorEndRef.current.style.right = activeItemRightOffset - 50 + "px";
-
-						setTimeout(() => {
-							if (!activeIndicatorStartRef.current || !activeIndicatorEndRef.current) return;
-							activeIndicatorEndRef.current.style.transition = "width 350ms ease";
-							activeIndicatorEndRef.current.style.width = "50px";
-
-							// 	setTimeout(() => {
-							// 		activeIndicatorEndRef.current.style.visibility = "hidden";
-							// 	}, 350);
-							// }, 350);
-						}, 350);
-					}, 350);
-				} else {
-					activeIndicatorStartRef.current.style.visibility = "visible";
-
-					activeIndicatorStartRef.current.style.right = previousItemRightOffset - 50 + "px";
-					activeIndicatorStartRef.current.style.width =
-						activeItemRightOffset - previousItemRightOffset - padding + 50 + "px";
-					setTimeout(() => {
-						if (!activeIndicatorStartRef.current || !activeIndicatorEndRef.current) return;
-
-						activeIndicatorStartRef.current.style.visibility = "hidden";
-						activeIndicatorEndRef.current.style.visibility = "visible";
-
-						activeIndicatorEndRef.current.style.width =
-							previousItemLeftOffset - activeItemLeftOffset + 50 + "px";
-						activeIndicatorEndRef.current.style.left = activeItemLeftOffset + "px";
-
-						setTimeout(() => {
-							if (!activeIndicatorStartRef.current || !activeIndicatorEndRef.current) return;
-
-							activeIndicatorEndRef.current.style.transition = "width 350ms ease";
-							activeIndicatorEndRef.current.style.width = "50px";
-						}, 350);
-					}, 350);
-				}
-
-				setPrevActiveItem(activeItem);
+				return;
 			}
+
+			const bottomNavgationWidth = bottomNavigationRef.current.offsetWidth;
+
+			const previousItemLeftOffset = prevActiveItem?.offsetLeft ?? 4;
+			const previousItemRightOffset = bottomNavgationWidth - (prevActiveItem?.offsetLeft || 0) || 4;
+
+			const activeItemLeftOffset = activeItem.offsetLeft;
+			const activeItemRightOffset = bottomNavgationWidth - (activeItem.offsetLeft || 0);
+
+			const padding = Number(bottomNavigationRef.current.style.padding);
+
+			console.log({
+				activeItemLink: activeItem.href,
+				previousItemLeftOffset,
+				previousItemRightOffset,
+				activeItemLeftOffset,
+				activeItemRightOffset,
+			});
+
+			const direction =
+				activeItem.offsetLeft > activeIndicatorRef.current.offsetLeft ? "right" : "left";
+
+			// move active indicator (don`t touch it)
+			activeIndicatorRef.current.style.visibility = "visible";
+			activeIndicatorRef.current.style.left = activeItem.offsetLeft + "px";
+
+			// initial
+			activeIndicatorStartRef.current.style.left = "unset";
+			activeIndicatorStartRef.current.style.right = "unset";
+			activeIndicatorEndRef.current.style.left = "unset";
+			activeIndicatorEndRef.current.style.right = "unset";
+
+			activeIndicatorStartRef.current.style.visibility = "hidden";
+			activeIndicatorStartRef.current.style.width = 50 + "px";
+			activeIndicatorEndRef.current.style.visibility = "hidden";
+			activeIndicatorEndRef.current.style.width = 50 + "px";
+			activeIndicatorEndRef.current.style.transition = "none";
+
+			if (direction === "right") {
+				activeIndicatorStartRef.current.style.visibility = "visible";
+
+				activeIndicatorStartRef.current.style.left = previousItemLeftOffset + "px";
+				activeIndicatorStartRef.current.style.width =
+					activeItemLeftOffset - previousItemLeftOffset - padding + 50 + "px";
+
+				setTimeout(() => {
+					if (!activeIndicatorStartRef.current || !activeIndicatorEndRef.current) return;
+
+					console.log(activeItem.offsetLeft);
+					activeIndicatorStartRef.current.style.visibility = "hidden";
+					activeIndicatorEndRef.current.style.visibility = "visible";
+
+					activeIndicatorEndRef.current.style.width =
+						previousItemRightOffset - activeItemRightOffset + 50 + "px";
+					activeIndicatorEndRef.current.style.right = activeItemRightOffset - 50 + "px";
+
+					setTimeout(() => {
+						if (!activeIndicatorStartRef.current || !activeIndicatorEndRef.current) return;
+						activeIndicatorEndRef.current.style.transition = "width 350ms ease";
+						activeIndicatorEndRef.current.style.width = "50px";
+
+						// 	setTimeout(() => {
+						// 		activeIndicatorEndRef.current.style.visibility = "hidden";
+						// 	}, 350);
+						// }, 350);
+					}, 350);
+				}, 350);
+			} else {
+				activeIndicatorStartRef.current.style.visibility = "visible";
+
+				activeIndicatorStartRef.current.style.right = previousItemRightOffset - 50 + "px";
+				activeIndicatorStartRef.current.style.width =
+					activeItemRightOffset - previousItemRightOffset - padding + 50 + "px";
+				setTimeout(() => {
+					if (!activeIndicatorStartRef.current || !activeIndicatorEndRef.current) return;
+
+					activeIndicatorStartRef.current.style.visibility = "hidden";
+					activeIndicatorEndRef.current.style.visibility = "visible";
+
+					activeIndicatorEndRef.current.style.width =
+						previousItemLeftOffset - activeItemLeftOffset + 50 + "px";
+					activeIndicatorEndRef.current.style.left = activeItemLeftOffset + "px";
+
+					setTimeout(() => {
+						if (!activeIndicatorStartRef.current || !activeIndicatorEndRef.current) return;
+
+						activeIndicatorEndRef.current.style.transition = "width 350ms ease";
+						activeIndicatorEndRef.current.style.width = "50px";
+					}, 350);
+				}, 350);
+			}
+
+			setPrevActiveItem(activeItem);
 		}
 	}, [currentUrl]);
 
